4d816cac126cabca5f9abf6c90e1b50bfb9c5ce2
Sollace <sollacea@gmail.com>
2016-06-19 10:04:02 +0200

    Improved the home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @month = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_month..1.week.ago).order(:created_at).reverse_order().limit(16)
-    @week = Video.where(hidden: false, created_at: 1.week.ago..Time.zone.now.yesterday.beginning_of_day).order(:created_at).reverse_order().limit(16)
-    @yesterday = Video.where(hidden: false, created_at: Time.zone.now.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day).order(:created_at).reverse_order().limit(32)
-    @today = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(:created_at).reverse_order().limit(32)
+    @popular = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_day).order(:score, :views).reverse_order.limit(4)
+    @month = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_month..1.week.ago).order(:created_at).reverse_order.limit(8)
+    @week = Video.where(hidden: false, created_at: 1.week.ago..Time.zone.now.yesterday.beginning_of_day).order(:created_at).reverse_order.limit(16)
+    @yesterday = Video.where(hidden: false, created_at: Time.zone.now.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day).order(:created_at).reverse_order.limit(32)
+    @today = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(:created_at).reverse_order.limit(32)
   end
 end
