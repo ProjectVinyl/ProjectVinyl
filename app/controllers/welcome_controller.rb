0048ba48bf114e2c55e8a8a26755bcfdd3dd8688
Sollace <sollacea@gmail.com>
2016-07-04 07:58:11 +0200

    Improve the popular videos selection

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @popular = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_day).order(:score, :views).reverse_order.limit(4)
+    @popular = Video.where(hidden: false, created_at: (Date.today - 90)..Time.zone.now.end_of_day).where('views > ? OR score > ?', 0, 0).order(:score, :views).reverse_order.limit(4)
     @month = Video.where(hidden: false, created_at: Time.zone.now.beginning_of_month..1.week.ago).order(:created_at).reverse_order.limit(8)
     @week = Video.where(hidden: false, created_at: 1.week.ago..Time.zone.now.yesterday.beginning_of_day).order(:created_at).reverse_order.limit(16)
     @yesterday = Video.where(hidden: false, created_at: Time.zone.now.yesterday.beginning_of_day..Time.zone.yesterday.end_of_day).order(:created_at).reverse_order.limit(32)
