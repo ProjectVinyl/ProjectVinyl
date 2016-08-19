6c9a3de568e200e895c215587749329c5eb32b18
Sollace <sollacea@gmail.com>
2016-08-19 19:36:25 +0200

    Properly set running flag to false when a worker stops/dies Restart workers if they're not running and a new video is created Removed the master thread from the video processor since it's not strictly needed any more

--- lib/video_processor.rb
+++ lib/video_processor.rb
@@ -7,33 +7,39 @@ class ProcessingWorker < ActiveRecord::Base
     end
   end
   
-  def stop
+  def stop()
+    if self.running
+      self.message = "Shut Down"
+      if @e
+        self.message << " Error: " + @e.to_s + "<br>" + e.backtrace
+      end
+    end
     self.running = false
-    self.message = "Shut Down"
-    self.update_status("stopped")
+    self.status = "stopped"
+    self.save
   end
   
   def start
     self.message = "Waiting"
     self.save
   end
+  
+  def exception=(e)
+    @e = e
+  end
 end
 
 class VideoProcessor
   @@flag = true
-  @@master = nil
-  
-  @@Processors = []
+  @@required_worker_count = 2
   @@SleepIncriment = 15
   @@SleepTimer = 0
   
   def self.status
     workers = ProcessingWorker.all
     result = "<div>Control flag: " + @@flag.to_s + "</div>"
-    
     result << "<div>Videos in queue: " + VideoProcessor.queue.length.to_s + "</div>"
-    
-    result = result + "<div>Workers: " + workers.length.to_s + "</div>"
+    result << "<div>Workers: " + workers.length.to_s + "</div>"
     workers.each do |worker|
       result << '<div>Thread #' + worker.id.to_s + ", status: " + worker.status + ", message: " + worker.message + '</div>'
     end
@@ -43,6 +49,7 @@ class VideoProcessor
   def self.enqueue(video)
     if !video.checkIndex && @@master && @@master.status
       puts "[Processing Manager] Enqueued video #" + video.id
+      ProcessingManager.startManager
     end
   end
   
@@ -54,13 +61,12 @@ class VideoProcessor
     return VideoProcessor.queue.first
   end
   
-  def self.processor(id)
-    db_object = ProcessingWorker.where(id: id).first
+  def self.processor(db_object)
     if db_object && db_object.running
       return
     end
     if !db_object
-      db_object = ProcessingWorker.create(id: id, running: true, status: "running", message: "Waiting")
+      db_object = ProcessingWorker.create(running: true, status: "running", message: "Waiting")
     else
       db_object.start
     end
@@ -85,11 +91,12 @@ class VideoProcessor
           end
         end
       rescue Exception => e
-        db_object.stop()
+        db_object.exception = e
         puts "[Processing Manager] Thread died #(" + index.to_s + ")"
         puts e
         puts e.backtrace
       ensure
+        db_object.stop(nil)
         ActiveRecord::Base.connection.close
       end
     }
@@ -101,34 +108,19 @@ class VideoProcessor
   
   def self.startManager
     puts "[Processing Manager] Attempting Master thread start"
-    if ProcessingWorker.where(running: true).length > 0
-      return false
+    result = 0
+    count = ProcessingWorker.all.count
+    while count < @@required_worker_count
+      VideoProcessor.processor(nil)
+      count += 1
+      result += 1
     end
-    puts "[Processing Manager] Starting Master..."
-    @@master = Thread.start {
-      begin
-        puts "[Processing Manager] Master Started"
-        while @@flag
-          while @@Processors.length < 2
-            @@Processors << VideoProcessor.processor(@@Processors.length + 1)
-          end
-          @@Processors.each_with_index do |thread,index|
-            if thread && thread.status == false
-              @@Processors[index] = VideoProcessor.processor(index)
-            end
-          end
-        end
-      rescue Exception => e
-        puts e
-        puts e.backtrace
-      ensure
-        @@master = nil
-        @@flag = false
-        ProcessingWorker.update_all(running: false, message: "Shut Down", status: "stopped")
-        ActiveRecord::Base.connection.close
-        puts "[Processing Manager] Master Shutting Down"
+    ProcessingWorker.where(running: false).each do |i|
+      if result < @@required_worker_count
+        VideoProcessor.processor(i)
       end
-    }
-    return true
+      result += 1
+    end
+    return result
   end
 end
