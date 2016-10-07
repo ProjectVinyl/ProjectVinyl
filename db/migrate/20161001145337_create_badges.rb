7199fc38779551534fb98d412ad9b5cbc3d04a36
Sollace <sollacea@gmail.com>
2016-10-07 18:38:07 +0200

    Added more badges

--- db/migrate/20161001145337_create_badges.rb
+++ db/migrate/20161001145337_create_badges.rb
@@ -7,6 +7,11 @@ class CreateBadges < ActiveRecord::Migration
       t.integer :badge_type, default: 0
       t.timestamps null: false
     end
-    Badge.create(title: "Duck", colour: "yellow", icon: "duck", badge_type: 1)
+    Badge.create([
+			{title: "Duck", colour: "yellow", icon: "duck", badge_type: 1},
+			{title: "Bronze Bit", colour: "orange", icon: "bit", badge_type: 0},
+			{title: "Silver Bit", colour: "lightblue", icon: "bit", badge_type: 0},
+			{title: "Gold Bit", colour: "yellow", icon: "bit", badge_type: 0}
+		])
   end
 end
