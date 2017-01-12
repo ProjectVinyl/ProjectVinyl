4388e380ea86334d1d47202dee0f2fab96d2150f
Sollace <sollacea@gmail.com>
2017-01-12 20:06:54 +0200

    Fix preferences not saving correctly

--- config/initializers/prefable.rb
+++ config/initializers/prefable.rb
@@ -6,9 +6,18 @@ module Prefable
   def options
     result = self.public_send(options_source)
     if !result || result.empty?
-      self.public_send((options_source.to_s + '=').to_sym, default_options)
-      self.save
+      self.update_prefs_column(default_options).save
       return self.public_send(options_source)
+    end
+    dirty = false
+    default_options.keys.each do |key|
+      if !result.key?(key)
+        result[key] = default_options[key]
+        dirty = true
+      end
+    end
+    if dirty
+      self.update_prefs_column(result).save
     end
     return result
   end
@@ -23,15 +32,25 @@ module Prefable
   end
   
   def set_option(key, val)
+    puts 'Called @owner.set_option with ' + { :ket => key, :value => val }.to_s
     key = key.to_sym
     if default_options.key?(key)
-      if options[key] == true || options[key] == false && !(val == true || val == false)
-        val = val.downcase
-        options[key] = val == 'true' || val == '1'
+      ops = options
+      if default_options[key] == true || default_options[key] == false
+        if !(val == true || val == false)
+          val = val.downcase
+          val = val == 'true' || val == '1'
+        end
+        puts 'Assign ' + key.to_s + ' = ' + val.to_s
+        ops[key] = val
+        puts 'Assigned ops[' + key.to_s + '] => ' + ops[key].to_s
       else
-        options[key] = val
+        puts 'Assign ' + key.to_s + ' = "' + val.to_s + '"'
+        ops[key] = val
       end
-      self.public_send((options_source.to_s + '=').to_sym, options)
+      puts 'Assigned ops[' + key.to_s + '] => ' + ops[key].to_s
+      puts 'public_send ' + ops.to_s
+      self.update_prefs_column(ops)
     end
   end
   
@@ -56,11 +75,17 @@ module Prefable
     end
     
     def save(hash)
+      puts 'Called Preferences.save with ' + hash.to_s
       hash.keys.each do |key|
         @owner.set_option(key, hash[key])
       end
       @owner.save
     end
+  end
+  
+  def update_prefs_column(hash)
+    self.public_send((options_source.to_s + '=').to_sym, hash)
+    return self
   end
 end
 
