3efd136b638c3854d507210b87500b15ed8c2a1e
Sollace <sollacea@gmail.com>
2017-03-04 21:33:42 +0200

    Slightly altered the popular stories section to prefer newer stories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @all = Video.Finder.order(:created_at).reverse_order.limit(50)
     @comments = Comment.joins(:comment_thread).where("`comments`.hidden = false AND `comment_threads`.owner_type != 'Report' AND `comment_threads`.owner_type != 'Pm'").includes(:direct_user, :comment_thread).order(:created_at).reverse_order.limit(5)
-    @popular = Video.Finder.order(:heat).reverse_order.limit(4)
-    @featured = Video.where(featured: true, hidden: false).first
+    @popular = Video.Finder.order(:created_at, :updated_at, :heat).reverse_order.limit(4)
+    @featured = Video.Finder.where(featured: true).first
     @featured_album = Album.where('featured > 0').first
     if user_signed_in?
       @feed = TagSubscription.get_feed_items(current_user).limit(15)
