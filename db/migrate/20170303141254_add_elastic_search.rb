9e1893d90cc84a674bb8552c9b8b61f81667038b
Sollace <sollacea@gmail.com>
2017-03-05 20:37:42 +0200

    Remove redundant delete

--- db/migrate/20170303141254_add_elastic_search.rb
+++ db/migrate/20170303141254_add_elastic_search.rb
@@ -2,7 +2,6 @@ class AddElasticSearch < ActiveRecord::Migration
   def change
     Video.__elasticsearch__.create_index!
     Video.import
-    User.__elasticsearch__.delete_index!
     User.__elasticsearch__.create_index!
     User.import
   end
