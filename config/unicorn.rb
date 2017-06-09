197d96babe421b94a9bd6366bf585de27377064d
Liam P. White <byteslice@airmail.cc>
2017-06-09 19:49:49 +0000

    deployment changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 # Set unicorn options
@@ -9,11 +8,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", backlog: 64
+listen "#{app_dir}/tmp/sockets/unicorn.sock", backlog: 64
 
 # Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
