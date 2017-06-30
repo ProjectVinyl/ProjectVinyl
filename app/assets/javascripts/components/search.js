6ae5790db3cc51246fa6b39db73a2bfba8a02156
Sollace <sollacea@gmail.com>
2017-06-30 22:53:07 +0200

    Fix errors being thrown by the search input code

--- app/assets/javascripts/components/search.js
+++ app/assets/javascripts/components/search.js
@@ -1,29 +1,35 @@
+import { jSlim } from '../utils/jslim.js';
+
 function setupSearch() {
-  const searchInput = document.querySelector('#search input'),
-        searchSelect = document.querySelector('#search select'),
-        searchType = document.querySelector('#search_type'),
-        searchTags = document.querySelector('#search_tags');
-
-  searchSelect.addEventListener('change', () => {
-    const value = searchSelect.value;
-    if (value === '0' || value === '2') {
-      searchInput.setAttribute('name', 'tagquery');
-      searchInput.setAttribute('placeholder', 'Tag Search');
-    } else {
-      searchInput.setAttribute('name', 'query');
-      searchInput.setAttribute('placeholder', 'Search');
-    }
-  });
-
-  searchType && searchType.addEventListener('change', () => {
-    const value = searchType.value;
-    if (value === '0' || value === '2') {
-      searchTags.style.display = '';
-    } else {
-      searchTags.style.display = 'none';
-    }
-  });
+  var searchInput = document.querySelector('#search input');
+  var searchSelect = document.querySelector('#search select');
+  
+  var searchType = document.getElementById('search_type');
+  var searchTags = document.getElementById('search_tags');
+  
+  if (searchSelect) {
+    searchSelect.addEventListener('change', function() {
+      var value = searchSelect.value;
+      if (value === '0' || value === '2') {
+        searchInput.name = 'tagquery';
+        searchInput.placeholder = 'Tag Search';
+      } else {
+        searchInput.name = 'query';
+        searchInput.placeholder = 'Search';
+      }
+    });
+  }
+  
+  if (searchType) {
+    searchType.addEventListener('change', function() {
+      var value = searchType.value;
+      if (value === '0' || value === '2') {
+        searchTags.style.display = '';
+      } else {
+        searchTags.style.display = 'none';
+      }
+    });
+  }
 }
 
-if (document.readyState !== 'loading') setupSearch();
-else document.addEventListener('DOMContentLoaded', setupSearch);
+jSlim.ready(setupSearch);
