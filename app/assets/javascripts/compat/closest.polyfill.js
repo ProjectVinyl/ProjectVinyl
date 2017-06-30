c78a3c15a0c1efcedc3e73d44b14f68e42ef4ebd
Sollace <sollacea@gmail.com>
2017-06-30 10:15:02 +0200

    Whitespace and... huh?

--- app/assets/javascripts/compat/closest.polyfill.js
+++ app/assets/javascripts/compat/closest.polyfill.js
@@ -2,30 +2,26 @@
 
 if (typeof Element.prototype.matches !== 'function') {
   Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || function matches(selector) {
-    var element = this;
-    var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+    var elements = (this.document || this.ownerDocument).querySelectorAll(selector);
     var index = 0;
-
-    while (elements[index] && elements[index] !== element) {
-      ++index;
+    while (elements[index]) {
+      if (elements[index++] === this) {
+        return true;
+      }
     }
-
-    return Boolean(elements[index]);
+    return false;
   };
 }
 
 if (typeof Element.prototype.closest !== 'function') {
   Element.prototype.closest = function closest(selector) {
     var element = this;
-
     while (element && element.nodeType === 1) {
       if (element.matches(selector)) {
         return element;
       }
-
       element = element.parentNode;
     }
-
     return null;
   };
 }
