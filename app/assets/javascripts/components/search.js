b7c87260ffa945c62f33e293d55938c351d42052
Liam P. White <byteslice@airmail.cc>
2017-06-30 18:29:00 +0000

    dejqueryize: search

--- app/assets/javascripts/components/search.js
+++ app/assets/javascripts/components/search.js
@@ -1,22 +1,29 @@
-$(function() {
-  var searchInput = $('#search input');
-  $('#search select').on('change', function() {
-    var val = this.value;
-    if (val == '0' || val == '2') {
-      searchInput.attr({
-        name: 'tagquery',
-        placeholder: 'Tag Search'
-      });
+function setupSearch() {
+  const searchInput = document.querySelector('#search input'),
+        searchSelect = document.querySelector('#search select'),
+        searchType = document.querySelector('#search_type'),
+        searchTags = document.querySelector('#search_tags');
+
+  searchSelect.addEventListener('change', () => {
+    const value = searchSelect.value;
+    if (value === '0' || value === '2') {
+      searchInput.setAttribute('name', 'tagquery');
+      searchInput.setAttribute('placeholder', 'Tag Search');
     } else {
-      searchInput.attr({
-        name: 'query',
-        placeholder: 'Search'
-      });
+      searchInput.setAttribute('name', 'query');
+      searchInput.setAttribute('placeholder', 'Search');
     }
   });
-  
-  $('#search_type').on('change', function() {
-    var val = this.value;
-    $('#search_tags').css('display', val == '0' || val == '2' ? '' : 'none');
+
+  searchType && searchType.addEventListener('change', () => {
+    const value = searchType.value;
+    if (value === '0' || value === '2') {
+      searchTags.style.display = '';
+    } else {
+      searchTags.style.display = 'none';
+    }
   });
-});
+}
+
+if (document.readyState !== 'loading') setupSearch();
+else document.addEventListener('DOMContentLoaded', setupSearch);
