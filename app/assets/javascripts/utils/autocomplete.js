298ac990801d72b965c2c1eea0e2c49219e8c77d
Liam P. White <byteslice@airmail.cc>
2017-07-03 01:29:11 +0000

    dejqueryize: autocomplete

--- app/assets/javascripts/utils/autocomplete.js
+++ app/assets/javascripts/utils/autocomplete.js
@@ -1,44 +1,47 @@
 import { ajax } from './ajax.js';
+import { jSlim } from './jslim.js';
 
-var autocomplete = null;
+let autocomplete = null;
 
 function lookup(sender, popout, action, input, validate) {
   ajax.post(action + '/lookup', {
-    query: input.val(), validate: validate ? 1 : 0
+    query: input.value, validate: validate ? 1 : 0
   }).json(function(json) {
-    popout.empty();
-    for (var i = 0; i < json.content.length; i++) {
-      var item = $('<li></li>');
-      item.text(json.content[i][1] + ' (#' + json.content[i][0] + ')');
-      item.attr('data-name', json.content[i][1]);
-      item.on('mousedown', function() {
-        input.val($(this).attr('data-name'));
-        sender.removeClass('pop-out-shown');
+    popout.innerHTML = '';
+    for (let i = 0; i < json.content.length; i++) {
+      let item = document.createElement('li');
+      item.textContext = `${json.content[i][1]} (#${json.content[i][0]})`;
+      item.dataset.name = json.content[i][1];
+      item.addEventListener('mousedown', () => {
+        input.value = item.dataset.name;
+        sender.classList.remove('pop-out-shown');
       });
-      popout.append(item);
+      popout.appendChild(item);
     }
-    sender[json.content.length ? 'addClass' : 'removeClass']('pop-out-shown');
-    sender[json.reject ? 'addClass' : 'removeClass']('invalid');
+    sender.classList[json.content.length ? 'add' : 'remove']('pop-out-shown');
+    sender.classList[json.reject ? 'add' : 'remove']('invalid');
   });
 }
 
-$(document).on('focus', '.auto-lookup:not(.loaded) input', function() {
-  var input = $(this);
-  var me = input.parent();
-  var popout = me.find('.pop-out');
-  var action = me.attr('data-action');
-  var lastValue = null;
-  var validate = me.hasClass('validate');
+jSlim.on(document, 'focusin', '.auto-lookup:not(.loaded) input', function() {
+  const input = this;
+  const me = input.parentNode;
+  const popout = me.querySelector('.pop-out');
+  const action = me.dataset.action;
+  const validate = me.classList.contains('validate');
+  let lastValue = null;
   
-  me.addClass('loaded');
-  input.on('blur', function() {
+  me.classList.add('loaded');
+
+  input.addEventListener('blur', () => {
     clearInterval(autocomplete);
     autocomplete = null;
   });
-  input.on('focus', function() {
+
+  input.addEventListener('focus', () => {
     if (!autocomplete) {
-      autocomplete = setInterval(function() {
-        var value = input.val();
+      autocomplete = setInterval(() => {
+        let value = input.value;
         if (value != lastValue) {
           lastValue = value;
           lookup(me, popout, action, input, validate);
