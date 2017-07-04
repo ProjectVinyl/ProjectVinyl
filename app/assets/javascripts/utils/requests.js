3475d8dabf630ef44793a5e0e4a4dd5220ca1123
Liam P. White <byteslice@airmail.cc>
2017-07-04 16:42:18 +0000

    deduplicate requests

--- app/assets/javascripts/utils/requests.js
+++ app/assets/javascripts/utils/requests.js
@@ -7,14 +7,11 @@ function csrfToken() {
   return meta && meta.content;
 }
 
-function fetchJson(verb, endpoint, body) {
+function fetchInternal(verb, endpoint, body, headers) {
   const data = {
     method: verb,
     credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': csrfToken(),
-    },
+    headers
   };
 
   if (body) {
@@ -25,29 +22,23 @@ function fetchJson(verb, endpoint, body) {
   return fetch(endpoint, data);
 }
 
-function fetchHtml(verb, endpoint, body) {
-  const data = {
-    method: verb,
-    credentials: 'same-origin',
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-Token': csrfToken(),
-    },
-  };
-
-  if (body) {
-    body._method = verb;
-    data.body = JSON.stringify(body);
-  }
-
-  return fetch(endpoint, data);
+export function fetchHtml(verb, endpoint, body) {
+  return fetchInternal(verb, endpoint, body, {
+    'X-Requested-With': 'XMLHttpRequest',
+    'X-CSRF-Token': csrfToken(),
+  });
 }
 
-function handleError(response) {
+export function fetchJson(verb, endpoint, body) {
+  return fetchInternal(verb, endpoint, body, {
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': csrfToken(),
+  });
+}
+
+export function handleError(response) {
   if (!response.ok) {
     throw new Error('Received error from server');
   }
   return response;
 }
-
-export { fetchJson, fetchHtml, handleError };
