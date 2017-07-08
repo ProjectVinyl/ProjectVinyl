8e97837d750edbe97b90189a3a5d6d7fac68bd62
Sollace <sollacea@gmail.com>
2017-07-08 14:23:18 +0200

    Cleaned up a bunch of inline styles

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,21 +1,23 @@
 class ArtistController < ApplicationController
   def view
-    if @user = User.where(id: params[:id]).first
-      @tags = @user.tags.includes(:tag_type)
-      if @user.tag_id
-        @art = @user.tag.videos.listable
-        @art_count = @art.count
-        @art = Pagination.paginate(@art, 0, 8, true)
-      end
-      @modifications_allowed = user_signed_in? && (current_user.id == @user.id || current_user.is_staff?)
-      @videos = @modifications_allowed ? @user.videos.includes(:tags).where(duplicate_id: 0) : @user.videos.listable
-      @videos = Pagination.paginate(@videos, 0, 8, true)
-      @albums = @modifications_allowed ? @user.albums : @user.albums.where('`albums`.hidden = false AND `albums`.listing = 0')
-      @albums = Pagination.paginate(@albums, 0, 8, true)
-      @comments = Comment.finder.joins(:comment_thread).select('`comments`.*').where("`comments`.user_id = ? AND `comment_threads`.id = comment_thread_id AND NOT `comment_threads`.owner_type = 'Report' AND NOT `comment_threads`.owner_type = 'Pm'", @user.id).order(:created_at).reverse_order.limit(3)
+    if !(@user = User.where(id: params[:id]).first)
+      return render '/layouts/error', locals: { title: 'Nothing to see here!', description: 'If there was an artist here they\'re probably gone now. ... sorry.' }
     end
+    
+    @tags = @user.tags.includes(:tag_type)
+    if @user.tag_id
+      @art = @user.tag.videos.listable
+      @art_count = @art.count
+      @art = Pagination.paginate(@art, 0, 8, true)
+    end
+    @modifications_allowed = user_signed_in? && (current_user.id == @user.id || current_user.is_staff?)
+    @videos = @modifications_allowed ? @user.videos.includes(:tags).where(duplicate_id: 0) : @user.videos.listable
+    @videos = Pagination.paginate(@videos, 0, 8, true)
+    @albums = @modifications_allowed ? @user.albums : @user.albums.where('`albums`.hidden = false AND `albums`.listing = 0')
+    @albums = Pagination.paginate(@albums, 0, 8, true)
+    @comments = Comment.finder.joins(:comment_thread).select('`comments`.*').where("`comments`.user_id = ? AND `comment_threads`.id = comment_thread_id AND NOT `comment_threads`.owner_type = 'Report' AND NOT `comment_threads`.owner_type = 'Pm'", @user.id).order(:created_at).reverse_order.limit(3)
   end
-
+  
   def update
     input = params[:user]
     if user_signed_in?
@@ -44,12 +46,12 @@ class ArtistController < ApplicationController
     end
     render 'layouts/error', locals: { title: 'Access Denied', description: "You can't do that right now." }
   end
-
+  
   def update_prefs
     current_user.prefs_cache.save(params[:settings]) if user_signed_in?
     redirect_to action: "edit", controller: "devise/registrations"
   end
-
+  
   def setbanner
     if user_signed_in? && (current_user.is_staff? || current_user.id == params[:id])
       if current_user.id == params[:id]
@@ -70,7 +72,7 @@ class ArtistController < ApplicationController
     end
     render status: 401, nothing: true
   end
-
+  
   def setavatar
     input = params[:user]
     if user_signed_in?
@@ -92,11 +94,10 @@ class ArtistController < ApplicationController
     end
     render 'layouts/error', locals: { title: 'Access Denied', description: "You can't do that right now." }
   end
-
+  
   def card
     if user = User.with_badges.where(id: params[:id]).first
-      render partial: '/layouts/artist_thumb_h', locals: { artist_thumb_h: user }
-      return
+      return render partial: '/layouts/artist_thumb_h', locals: { artist_thumb_h: user }
     end
     render status: 404, nothing: true
   end
@@ -108,18 +109,17 @@ class ArtistController < ApplicationController
       else
         @user = User.where(id: params[:id]).first
       end
-      render partial: 'banner'
-      return
+      return render partial: 'banner'
     end
     render status: 404, nothing: true
   end
-
+  
   def list
     @page = params[:page].to_i
     @results = Pagination.paginate(User.all.order(:created_at), @page, 50, true)
     render template: '/view/listing', locals: { type_id: 2, type: 'users', type_label: 'User', items: @results }
   end
-
+  
   def page
     @page = params[:page].to_i
     @results = Pagination.paginate(User.all.order(:created_at), @page, 50, true)
