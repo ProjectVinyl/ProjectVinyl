053f7d0720fb2a85c1c159050c380e0719a5b809
Sollace <sollacea@gmail.com>
2017-08-08 09:42:52 +0200

    Fixed cookie problems

--- app/assets/javascripts/utils/cookies.js
+++ app/assets/javascripts/utils/cookies.js
@@ -1,8 +1,18 @@
 import { tryUnmarshal } from './misc';
 
+function extractCookieValue(key) {
+  let cook = document.cookie;
+  var index = cook.lastIndexOf(key);
+  if (index < 0) return null;
+  cook = cook.substring(index, cook.length).split(';')[0];
+  index = cook.indexOf('=');
+  if (index < 0) return null;
+  return cook.substring(index + 1, cook.length).trim();
+}
+
 export const cookies = {
   get: function getCookie(key) {
-    key = document.cookie.replace(new RegExp('(?:(?:^|.*;\s*)' + key + '\s*=\s*([^;]*).*$)|^.*$'), '$1');
+    key = extractCookieValue(key);
     if (key) return tryUnmarshal(decodeURIComponent(key));
   },
   set: function setCookie(key, value) {
