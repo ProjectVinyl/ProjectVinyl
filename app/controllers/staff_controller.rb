abdf12c235123530a5dac1a83cd6960832be16a4
Sollace <sollacea@gmail.com>
2017-08-08 12:17:09 +0200

    Housekeeping

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,16 +1,4 @@
 class StaffController < ApplicationController
-  def index
-  end
-
-  def copyright
-  end
-
-  def policy
-  end
-
-  def donate
-  end
-  
   def ajax_donate
     render partial: "staff/donate"
   end
