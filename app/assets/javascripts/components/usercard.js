76456a35ef446ca3e2d92ff4ccc7bb83abdcea3f
Sollace <sollacea@gmail.com>
2017-08-08 13:30:14 +0200

    Cleaned up hovercards

--- app/assets/javascripts/components/usercard.js
+++ app/assets/javascripts/components/usercard.js
@@ -1,50 +1,42 @@
 import { jSlim } from '../utils/jslim';
 import { ajax } from '../utils/ajax';
 
-var hoverTimeout = null;
-function closeUsercard() {
-  jSlim.all('.hovercard.shown', function(card) {
-    card.classList.remove('shown');
-  });
-}
-
 function openUsercard(sender, usercard) {
-  closeUsercard();
-  sender.appendChild(usercard);
-  
-  if (hoverTimeout) {
-    clearTimeout(hoverTimeout);
-  }
-  setTimeout(function() {
-    usercard.classList.add('shown');
-    hoverTimeout = setTimeout(function() {
-      jSlim.all('.user-link:not(:hover) .hovercard.shown', function(a) {
-        a.classList.remove('shown');
-      });
-    }, 500);
+  const newUsercard = usercard.cloneNode(true);
+  sender.appendChild(newUsercard);
+  requestAnimationFrame(() => {
+    newUsercard.classList.add('shown');
+  });
+  setTimeout(() => {
+    usercard.parentNode.removeChild(usercard);
   }, 500);
 }
 
-jSlim.on(document, 'mouseover', '.user-link', function(e) {
-  if (e.target.closest('.user-link-ignore')) return;
-  
-  var id = this.dataset.id;
-  var usercard = document.querySelector('.hovercard[data-id="' + id + '"]');
-  if (!usercard) {
-    usercard = document.createElement('DIV');
-    usercard.classList.add('hovercard');
-    usercard.dataset.id = id;
-    usercard.addEventListener('mouseover', function(ev) {
-      ev.stopPropagation();
-    });
-    var self = this;
-    ajax.get('/users/' + id + '/hovercard').text(function(text) {
-      usercard.innerHTML = text;
-      openUsercard(self, usercard);
-    });
-  } else {
-    openUsercard(this, usercard);
-  }
-});
+function closeUsercard() {
+  jSlim.all('.user-link .hovercard.shown', a => {
+    a.classList.remove('shown');
+  });
+}
 
 jSlim.on(document, 'mouseout', '.user-link', closeUsercard);
+jSlim.on(document, 'mouseover', '.user-link', function(e) {
+  if (e.target.closest('.user-link-ignore')) return;
+  closeUsercard();
+  
+  const id = this.dataset.id;
+  
+  let usercard = document.querySelector('.hovercard[data-id="' + id + '"]');
+  if (usercard) {
+    return openUsercard(this, usercard);
+  }
+  
+  usercard = document.createElement('DIV');
+  usercard.classList.add('hovercard');
+  usercard.dataset.id = id;
+  this.appendChild(usercard);
+  
+  ajax.get('users/' + id + '/hovercard').text(text => {
+    usercard.innerHTML = text;
+    usercard.classList.add('shown');
+  });
+});
