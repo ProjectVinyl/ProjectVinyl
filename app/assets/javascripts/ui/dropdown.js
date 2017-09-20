ef91ef5eeb16f2bde528fadfd49f02509a6358a2
Sollace <sollacea@gmail.com>
2017-09-20 09:35:24 +0200

    Code style and cleanup

--- app/assets/javascripts/ui/dropdown.js
+++ app/assets/javascripts/ui/dropdown.js
@@ -1,59 +1,44 @@
 import { jSlim } from '../utils/jslim';
 
-const Popout = {
-  toggle: function(sender) {
-    if (sender && !sender.classList.contains('pop-out-shown')) {
-      this.show(sender);
-    } else {
-      this.hide(sender);
-    }
-  },
-  show: function(sender) {
-    var content = sender.querySelector('.pop-out');
-    const left = jSlim.offset(content).left;
-    
-    this.hideAll();
-    sender.classList.add('pop-out-shown');
-    sender.classList.remove('pop-left');
-    sender.classList.remove('pop-right');
-    
-    if (left + content.clientWidth > document.documentElement.clientWidth) {
-      sender.classList.add('pop-left');
-    }
-    if (left < 0) {
-      sender.classList.add('pop-right');
-    }
-  },
-  hide: function(sender) {
-    sender.classList.remove('pop-out-shown');
-  },
-  hideAll: function() {
-    jSlim.all('.pop-out-shown:not(:hover)', this.hide);
-  }
-};
+function hide(sender) {
+	sender.classList.remove('pop-out-shown');
+}
 
-jSlim.on(document, 'touchstart', '.drop-down-holder:not(.hover), .mobile-touch-toggle:not(.hover)', function(e) {
+document.addEventListener('mousedown', () => jSlim.all('.pop-out-shown:not(:hover)', hide));
+jSlim.on(document, 'click', '.popper .pop-out-toggle', (e, target) => {
+  if (e.which != 1) return;
+	e.preventDefault();
+	
+  target = target.closest('.popper');
+	if (target.classList.contains('pop-out-shown')) {
+		return hide(sender);
+	}
+	
+	const content = target.querySelector('.pop-out');
+	const left = jSlim.offset(content).left;
+	
+	target.classList.add('pop-out-shown');
+	target.classList.toggle('pop-left', left + content.clientWidth > document.documentElement.clientWidth);
+	target.classList.toggle('pop-right', left < 0);
+});
+
+jSlim.on(document, 'touchstart', '.drop-down-holder:not(.hover), .mobile-touch-toggle:not(.hover)', (e, target) => {
   e.preventDefault();
-  
-  var self = this;
+  target.classList.add('hover');
+	
   // ffs https://www.chromestatus.com/features/5093566007214080
-  
   ['touchstart', 'touchmove'].forEach(t => {
-    this.addEventListener(t, clos, { passive: false });
+    target.addEventListener(t, clos, { passive: false });
     document.addEventListener(t, clos, { passive: false });
   });
   
-  this.classList.add('hover');
-  
   function clos(e) {
-    if (e.target.closest('a')) {
-      return; // Links should only be triggered on touch when the drop-down is already open
-    }
+    if (e.target.closest('a')) return; // Links should only be triggered on touch when the drop-down is already open
     
-    self.classList.remove('hover');
+    target.classList.remove('hover');
     
-    ['touchstart', 'touchmove'].forEach(function(t) {
-      self.removeEventListener(t, clos);
+    ['touchstart', 'touchmove'].forEach(t => {
+      target.removeEventListener(t, clos);
       document.removeEventListener(t, clos);
     });
     
@@ -61,13 +46,3 @@ jSlim.on(document, 'touchstart', '.drop-down-holder:not(.hover), .mobile-touch-t
     e.stopPropagation();
   }
 }, { passive: false });
-
-jSlim.on(document, 'click', '.pop-out-toggle', function(e) {
-  if (e.which != 1) return;
-  Popout.toggle(this.closest('.popper'));
-  e.preventDefault();
-});
-
-document.addEventListener('mousedown', function() {
-  Popout.hideAll();
-});
