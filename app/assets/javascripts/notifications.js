8fc40292f320f9c443cdbc9f1eaf072d4e39a734
Sollace <sollacea@gmail.com>
2017-09-28 14:19:22 +0200

    Attempt to fix notifications

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,31 +1,35 @@
-const counters = { feeds: 0, notices: 0, mail: 0 };
-const keys = Object.keys(counters);
+var counters = { feeds: 0, notices: 0, mail: 0 };
+var keys = Object.keys(counters);
 
-const stepDelayIncrement = 15000;
+var stepDelayIncrement = 15000;
 
-let stepDelay = 30000;
-let controlFlag = null;
-let ports = [];
+var stepDelay = 30000;
+var controlFlag = null;
+var ports = [];
 
-const recievers = {
-  connect: (port, msg) => {
-    keys.forEach(key => counters[key] = msg[key]);
+var self = this;
+
+var recievers = {
+  connect: function(port, msg) {
+    keys.forEach(function(key) {
+      counters[key] = msg[key];
+    });
     ports.push(port);
     if (ports.length == 1) controlFlag = setTimeout(heartbeat, stepDelay * 2);
   },
-  disconnect: (port, msg) => {
+  disconnect: function(port, msg) {
     if (!ports.length) return;
     ports.splice(ports.indexOf(port), 1);
     if (!ports.length && controlFlag) {
       clearTimeout(controlFlag);
-      this.close();
+      self.close();
     }
   }
 };
 
 function heartbeat() {
   const request = new XMLHttpRequest();
-  request.onreadystatechange = () => {
+  request.onreadystatechange = function() {
     if (request.readyState == XMLHttpRequest.DONE) {
       if (request.status == 200) {
         sendMessage(JSON.parse(request.responseText));
@@ -38,18 +42,18 @@ function heartbeat() {
       }
     }
   };
-  let url = [];
-  keys.forEach(key => {
-    if (counters[key] !== undefined) url.push(`${key}=${counters[key]}`);
+  var url = [];
+  keys.forEach(function(key) {
+    if (counters[key] !== undefined) url.push(key + '=' + counters[key]);
   });
-  request.open('GET', `/ajax/notifications?${url.join('&')}`, true);
+  request.open('GET', '/ajax/notifications?' + url.join('&'), true);
   request.send();
 }
 
 function sendMessage(msg) {
-  ports = ports.filter(port => {
+  ports = ports.filter(function(port) {
     try {
-      keys.forEach(counter => {
+      keys.forEach(function(counter) {
         if (msg[counter] !== undefined && msg[counter] != counters[counter]) {
           port.postMessage({ command: counter, count: msg[counter] });
           counters[counter] = msg[counter];
@@ -62,9 +66,9 @@ function sendMessage(msg) {
   });
 }
 
-this.addEventListener('connect', e => {
-  const port = e.ports[0];
-  port.addEventListener('message', e => {
+this.addEventListener('connect', function(e) {
+  var port = e.ports[0];
+  port.addEventListener('message', function(e) {
     if (receivers[e.data.command]) receivers[e.data.command](port, e.data);
   });
   port.start();
