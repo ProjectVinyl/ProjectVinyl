85e3d75dfd70ae0910f64fc0f75860b3dcf064fd
Sollace <sollacea@gmail.com>
2017-09-28 12:53:02 +0200

    Cleanup contextmenus

--- app/assets/javascripts/ui/contextmenu.js
+++ app/assets/javascripts/ui/contextmenu.js
@@ -1,15 +1,13 @@
 import { all, offset } from '../jslim/dom';
-import { ready } from '../jslim/events';
 
 export function ContextMenu(dom, container) {
   this.dom = dom;
   this.container = container || document.body;
   this.container.addEventListener('contextmenu', e => this.show(e));
 }
-
 ContextMenu.prototype = {
   addItem: function(title, initial, callback) {
-    this.dom.insertAdjacentHTML('beforeend', '<li><div class="label">' + title + '</div><div class="value"></div></li>');
+    this.dom.insertAdjacentHTML('beforeend', `<li><div class="label">${title}</div><div class="value"></div></li>`);
     this.dom.lastChild.addEventListener('click', () => callback(val));
     
     const item = this.dom.lastChild.querySelector('.value');
@@ -22,6 +20,8 @@ ContextMenu.prototype = {
     val(initial);
   },
   show: function(ev) {
+    ev.preventDefault();
+    
     let x = ev.clientX, y = ev.clientY;
     
     if (x + this.dom.offsetWidth >= document.body.clientWidth) {
@@ -35,29 +35,23 @@ ContextMenu.prototype = {
     x += document.scrollingElement.scrollLeft - off.left;
     y += document.scrollingElement.scrollTop - off.top;
     
-    this.dom.style.top = y + 'px';
-    this.dom.style.left = x + 'px';
+    this.dom.style.top = `${y}px`;
+    this.dom.style.left = `${x}px`;
     this.dom.classList.remove('hidden');
-    
-    ev.preventDefault();
   },
   hide: function(ev) {
-    if (ev.which === 1 && !this.dom.classList.contains('hidden')) {
-      this.dom.classList.add('hidden');
-      return 1;
-    }
-    return 0;
+    if (ev.which !== 1 || this.dom.classList.contains('hidden')) return;
+    this.dom.classList.add('hidden');
+    return true;
   }
 };
 
-ready(() => {
-  function hideAll() {
-    all('.contextmenu', p => p.classList.add('hidden'));
-  }
-  
-  window.addEventListener('resize', hideAll);
-  window.addEventListener('blur', hideAll);
-  document.addEventListener('click', ev => {
-    if (ev.button === 0) hideAll();
-  });
+function hideAll() {
+  all('.contextmenu', p => p.classList.add('hidden'));
+}
+
+window.addEventListener('resize', hideAll);
+window.addEventListener('blur', hideAll);
+document.addEventListener('click', ev => {
+  if (ev.button === 0) hideAll();
 });
