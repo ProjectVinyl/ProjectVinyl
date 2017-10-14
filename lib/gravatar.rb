48c4cf62416bf5d1bca2ba31a869144a32052cdf
Sollace <sollacea@gmail.com>
2017-10-14 12:52:57 +0200

    Fixed gravatar avatars not showing correctly

--- lib/gravatar.rb
+++ lib/gravatar.rb
@@ -4,7 +4,7 @@ module Gravatar
   def self.avatar_for(email, params)
     email = Digest::MD5.hexdigest((email || '').strip.downcase)
     params = params.to_param
-    query = params.empty? ? '?' + params : ''
+    query = params.empty? ? '' : '?' + params
     "//www.gravatar.com/avatar/#{email}.png#{query}"
   end
 end
