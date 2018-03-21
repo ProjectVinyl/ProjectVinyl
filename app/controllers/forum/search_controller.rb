6725010643ca9c1aaa46aaf487d2b4dc9bd1cbd5
Sollace <sollacea@gmail.com>
2018-03-21 19:03:27 +0200

    Improved the forum search

--- app/controllers/forum/search_controller.rb
+++ app/controllers/forum/search_controller.rb
@@ -6,26 +6,22 @@ module Forum
       @text_query = params[:text_query]
       @category = (params[:category] || 0).to_i
       
-      @q = []
+      @q = Comment.visible.where('`comment_threads`.owner_type = "Board"').order(:updated_at, :created_at).with_likes(current_user)
       
-      if @title_query || @poster_query || @text_query || (@category > 0)
-        @q = Comment.visible.where('`comment_threads`.owner_type = "Board"').order(:updated_at, :created_at).with_likes(current_user)
-        
-        if @title_query
-          @q = @q.where('`comment_threads`.title LIKE ?', "%#{@title_query}%")
-        end
-        
-        if @poster_query
-          @q = @q.joins(:direct_user).where('`users`.username LIKE ?', "%#{@poster_query}%")
-        end
-        
-        if @text_query
-          @q = @q.where('bbc_content LIKE ?', "%#{@text_query}%")
-        end
-        
-        if @category > 0
-          @q = @q.where('`comment_threads`.owner_id = ?', @category)
-        end
+      if @title_query
+        @q = @q.where('`comment_threads`.title LIKE ?', "%#{@title_query}%")
+      end
+      
+      if @poster_query
+        @q = @q.joins(:direct_user).where('`users`.username LIKE ?', "%#{@poster_query}%")
+      end
+      
+      if @text_query
+        @q = @q.where('bbc_content LIKE ?', "%#{@text_query}%")
+      end
+      
+      if @category > 0
+        @q = @q.where('`comment_threads`.owner_id = ?', @category)
       end
       
       @results = Pagination.paginate(@q, params[:page].to_i, 20, params[:order] != '1')
