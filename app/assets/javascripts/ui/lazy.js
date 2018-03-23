fbf9fdf45063d7e97cfc7e3717a1607af112fa3a
Sollace <sollacea@gmail.com>
2018-03-23 13:02:40 +0200

    Fixed the load-more buttons

--- app/assets/javascripts/ui/lazy.js
+++ app/assets/javascripts/ui/lazy.js
@@ -2,18 +2,17 @@ import { ajax } from '../utils/ajax';
 import { addDelegatedEvent } from '../jslim/events';
 
 addDelegatedEvent(document, 'click', '.load-more button', (e, button) => {
-  const page = parseInt(button.dataset.page) + 1;
+  if (button.classList.contains('working')) return;
+  
+  const page = ++button.dataset.page;
+  const target = document.getElementById(button.dataset.target);
   
   button.classList.add('working');
-  ajax.get(button.dataset.url, {
-    page: page
-  }).json(json => {
-    const target = document.getElementById(button.dataset.target);
-    
+  ajax.get(button.dataset.url, {page: page}).json(json => {
     button.classList.remove('working');
+    
     if (json.page == page) {
-      target.innerHTML += json.content;
-      button.dataset.page = page;
+      target.insertAdjacentHTML('beforeend', json.content);
     } else {
       button.parentNode.removeChild(button);
     }
