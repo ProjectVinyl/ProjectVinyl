79b221d8003cb579217e3ce2c40c53c15c119a92
Sollace <sollacea@gmail.com>
2018-03-23 23:38:40 +0200

    Typos

--- app/assets/javascripts/vendor/mozilla.js
+++ app/assets/javascripts/vendor/mozilla.js
@@ -1,1 +1,1 @@
-if (!/Mozilla.*Mobile/g.test(navigator.userAgent)) document.documentElement.classList.remove('andoid-mozilla');
+if (!/Mozilla.*Mobile/g.test(navigator.userAgent)) document.documentElement.classList.remove('android-mozilla');
