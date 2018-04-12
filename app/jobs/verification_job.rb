c31de4c7afce129cd73b4f66beb3a2069ed58264
Sollace <sollacea@gmail.com>
2018-04-13 00:28:40 +0200

    Syntax error

--- app/jobs/verification_job.rb
+++ app/jobs/verification_job.rb
@@ -4,7 +4,7 @@ class VerificationJob < ApplicationJob
   def perform(user_id)
     Report.report_on(
       "System Integrity Report #{Time.zone.now}",
-      "Action 'System Integrity Report' has been completed"
+      "Action 'System Integrity Report' has been completed",
       { user_id: user_id, first: "System", other: "Working..." }
     ) do |report|
       report.other = ""
