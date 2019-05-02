65d333ab024b946d25012db59b6b4135632ae03b
Sollace <sollacea@gmail.com>
2019-05-02 12:42:16 +0200

    Added a toggle to turn off Background Ponies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,18 @@ module ApplicationHelper
   include StringsHelper
   
   def self.read_only
-    ApplicationSettings.get(:readonly)
+    ApplicationSettings.get(:read_only)
   end
-  
+
   def read_only
     ApplicationHelper.read_only
+  end
+
+  def self.bg_ponies
+    ApplicationSettings.get(:bg_ponies)
+  end
+
+  def bg_ponies
+    ApplicationHelper.bg_ponies
   end
 end
