777d7cef8042de83c54e53a29319838c5b1fb2f1
Sollace <sollacea@gmail.com>
2019-05-06 15:39:35 +0200

    Fixed the donate popup on the main page

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,5 +1,5 @@
 class StaffController < ApplicationController
   def donate
-    render partial: 'donate' if params[:format] == 'json'
+    render partial: 'donate', formats: [:html] if params[:format] == 'json'
   end
 end
