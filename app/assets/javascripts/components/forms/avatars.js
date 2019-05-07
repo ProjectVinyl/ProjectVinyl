b40efa4ba8a66ab524b0f62382b3a2e951bced64
Sollace <sollacea@gmail.com>
2019-05-07 14:30:17 +0200

    Avatar preview should turn to the default avatar

--- app/assets/javascripts/components/forms/avatars.js
+++ app/assets/javascripts/components/forms/avatars.js
@@ -1,11 +1,22 @@
 import { addDelegatedEvent } from '../../jslim/events';
 import { all } from '../../jslim/dom';
 
-addDelegatedEvent(document, 'ajax:complete', 'form.js-avatar-select', (e, target) => {
+function getAvatarPreviewUrl(target) {
   const input = target.querySelector('.avatar.file-select input');
-  const ext = input.files.length ? input.files[0].name.split('.').reverse()[0] : 'png';
-  const img = `url(/avatar/${target.dataset.id}.${ext}?${new Date().getTime()})`;
 
-  all(target, '.preview', el => el.style.backgroundImage = img);
-  all('#login .avatar-wrapper.small span', el => el.style.backgroundImage = img);
+  if (!input.files.length) {
+    return '/images/default-avatar.png';
+  }
+
+  const ext = input.files.length ? input.files[0].name.split('.').reverse()[0] : 'png';
+
+  return `/avatar/${target.dataset.id}.${ext}?${new Date().getTime()}`;
+}
+
+addDelegatedEvent(document, 'ajax:complete', 'form.js-avatar-select', (e, target) => {
+  const src = `url(${getAvatarPreviewUrl(target)})`;
+
+  target.querySelector('.preview').style.backgroundImage = src;
+
+  all('#login .avatar-wrapper.small span', el => el.style.backgroundImage = src);
 });
