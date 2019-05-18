6eefb1c07583a9ec9dcb174b213d34f579d82802
Sollace <sollacea@gmail.com>
2019-05-18 12:01:25 +0200

    You can now interact with the featured video on the front page

--- app/assets/javascripts/components/vote.js
+++ app/assets/javascripts/components/vote.js
@@ -5,16 +5,24 @@ function count(me, offset, save) {
   me.classList.toggle('liked', offset > 0);
   
   let count = me.querySelector('.count');
+
   if (!count) {
     count = me.querySelector('span');
     count.innerHTML = '<span class="count"></span>';
     count = count.firstChild;
   }
-  
+
   const updateUI = state => {
     me.dataset.count = state.count;
     count.classList.toggle('hidden', state.count < 1);
     count.innerText = state.count;
+
+    const score = me.parentNode.querySelector('.score');
+    if (score) {
+      const likes = parseInt(me.parentNode.querySelector('.like').dataset.count);
+      const dislikes = parseInt(me.parentNode.querySelector('.dislike').dataset.count);
+      score.innerHTML = `<b>${likes - dislikes}</b>`;
+    }
   };
   
   updateUI({ count: (parseInt(me.dataset.count) || 0) + offset });
@@ -26,19 +34,25 @@ function save(sender, data) {
   return ajax.put(`${sender.dataset.target || 'videos'}/${sender.dataset.id}/${sender.dataset.action}`, data);
 }
 
-addDelegatedEvent(document, 'click', 'button.action.like, button.action.dislike', (e, target) => {
-  if (e.button) return;
+addDelegatedEvent(document, 'click', '.action.like, .action.dislike', (e, target) => {
+  if (e.button) {
+    return;
+  }
   if (!target.classList.contains('liked')) {
     let other = target.parentNode.querySelector('.liked');
-    if (other) count(other, -1);
+    if (other) {
+      count(other, -1);
+    }
   }
+
   const offset = target.classList.contains('liked') ? -1 : 1;
+
   save(target, {
     incr: offset
   }).json(count(target, offset));
 });
 
-addDelegatedEvent(document, 'click', 'button.action.star', (e, target) => {
+addDelegatedEvent(document, 'click', '.action.star', (e, target) => {
   if (e.button) return;
   target.classList.toggle('starred');
   save(target).json(json => {
