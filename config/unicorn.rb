ce832f5a6a39289a97b35eb8223bc8354281062c
Sollace <sollacea@gmail.com>
2019-07-07 16:26:37 +0200

    Change unicorn for the new directory

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # set path to application
-app_dir = "/home/vinylscratch/ProjectVinyl"
+app_dir = "/home/ubuntu/ProjectVinyl"
 working_directory app_dir
 
 # Set unicorn options
@@ -8,7 +8,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{app_dir}/tmp/sockets/unicorn.sock", backlog: 64
+listen "0.0.0.0:8080"
 
 # Logging
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
