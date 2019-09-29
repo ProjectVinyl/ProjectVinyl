3926d100ee9710e95aee1a70eb0b217b536b0ce8
Sollace <sollacea@gmail.com>
2019-09-29 18:37:04 +0200

    Miiigration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190929133247) do
+ActiveRecord::Schema.define(version: 20190929133248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
