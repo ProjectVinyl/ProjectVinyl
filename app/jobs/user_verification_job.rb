2a364042a21572b5354edd05d3f62fb7cca12fe6
Sollace <sollacea@gmail.com>
2019-09-29 20:43:27 +0200

    Move the integrity checks to the job classes

--- app/jobs/user_verification_job.rb
+++ app/jobs/user_verification_job.rb
@@ -8,7 +8,33 @@ class UserVerificationJob < ApplicationJob
       { user_id: user_id, first: "System", other: "Working..." }
     ) do |report|
       report.other = ""
-      Verification::UserVerification.verify_integrity(report)
+      verify_integrity(report)
     end
+  end
+
+  def verify_integrity(report)
+    avatars_reset = 0
+    banners_reset = 0
+
+    User.all.find_each do |u|
+      avatar_exists = File.exist?(u.avatar_path)
+
+      if u.mime.nil? != avatar_exists
+        u.mime = avatar_exists ? 'png' : nil
+        u.save
+        avatars_reset += 1
+      end
+
+      banner_exists = File.exist?(u.banner_path)
+
+      if u.banner_set != banner_exists
+        u.banner_set = banner_exists
+        u.save
+        banners_reset += 1
+      end
+    end
+
+    report.write("User avatars reset: #{avatars_reset}")
+    report.write("User banners reset: #{banners_reset}")
   end
 end
--- app/models/verification/video_verification.rb
+++ app/models/verification/video_verification.rb
@@ -36,56 +36,5 @@ module Verification
       Video.where('id IN (?)', workings).update_all(processed: false)
       Video.where(processed: nil, hidden: false).count # return count
     end
-
-    def self.build_file_list(location)
-      webms = []
-      sources = []
-
-      Dir.entries(location.to_s).each do |name|
-        next unless name.index('.')
-
-        split = name.split('.')
-        id = split[0].to_i
-
-        if id && id > 0
-          if split[1] == 'webm'
-            webms << id
-          else
-            sources << id
-          end
-        end
-      end
-
-      { webm: webms, sources: sources }
-    end
-
-    def self.find_missing_file_ids(files)
-      {
-        webm: Video.where('id NOT IN (?)', files[:sources].uniq),
-        sources: Video.where('audio_only = false AND id NOT IN (?)', files[:webms].uniq)
-      }
-    end
-
-    def self.verify_integrity(report)
-      public_files = self.build_file_list(Rails.root.join('public', 'stream'))
-      private_files = self.build_file_list(Rails.root.join('private', 'stream'))
-
-      public_files[:webms] += private_files[:webms]
-      public_files[:sources] += private_files[:sources]
-
-      lost_files = find_missing_file_ids(public_files)
-
-      total = Video.all.count
-
-      report.write("Missing video files: #{lost_files[:sources].count}")
-      lost_files[:sources].each do |v|
-        report.write("  #{v}")
-      end
-
-      report.write("Missing webm files : #{lost_files[:webms].count}")
-      lost_files[:webms].each do |v|
-        report.write("  #{v}")
-      end
-    end
   end
 end
