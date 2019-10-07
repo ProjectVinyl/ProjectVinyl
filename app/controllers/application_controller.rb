a3af955f6ee0a055a5ae3b38cc303845f3fac4c6
Sollace <sollacea@gmail.com>
2019-10-07 15:24:29 +0200

    Fix encorrect redirects when signing in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,20 @@
 class ApplicationController < ActionController::Base
   include Paginateable
   include Errorable
-  
+  include Trackable
+
   protect_from_forgery with: :exception
-  
-  before_action :set_start_time
+
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :poke_user, if: :user_signed_in?
-  after_action :store_last_location, if: :content_page?
-  
-  def content_page?
-    !devise_controller? && controller_name != "imgs" && action_name != "download" && (/\/(api|ajax|.json)\// =~ request.fullpath).nil?
-  end
-  
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
-  
-  def set_start_time
-    @start_time = Time.now
-  end
-  
-  def store_last_location
-    return unless request.get?
-    store_location_for('user', request.fullpath)
-  end
-  
+
   def anonymous_user_id
     user_signed_in? ? current_user.id : UserAnon.anon_id(session)
-  end
-  
-  def poke_user
-    current_user.poke
   end
 end
---
+++ app/controllers/concerns/trackable.rb
@@ -0,0 +1,25 @@
+module Trackable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_start_time
+    after_action :poke_user, if: :user_signed_in?
+  end
+
+  protected
+  def set_start_time
+    @start_time = Time.now
+  end
+
+  def poke_user
+    current_user.poke
+  end
+
+  def after_sign_in_path_for(resource)
+    request.referrer
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+end
