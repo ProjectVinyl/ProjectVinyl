37e737427303f37aa6b8ef8ea1ad4bbc6ab850bf
Sollace <sollacea@gmail.com>
2020-01-31 21:34:29 +0200

    Allow the coppa and tags flags from the front-end api

--- app/controllers/api/youtubes_controller.rb
+++ app/controllers/api/youtubes_controller.rb
@@ -14,7 +14,7 @@ module Api
       end
 
       succeed id: tube.video_id(@url),
-        attributes: tube.get(@url, include_hash([:title, :description, :artist, :thumbnail, :iframe, :source])),
+        attributes: tube.get(@url, include_hash([:title, :description, :artist, :thumbnail, :iframe, :source, :coppa, :tags])),
         meta: {
           url: @url
       }
