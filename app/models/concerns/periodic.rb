c78a8f455b889389579585eb0b4e6cccea26c07f
Sollace <sollacea@gmail.com>
2020-07-06 15:53:08 +0200

    Add links for each front-page section to a relevant search page

--- app/models/concerns/periodic.rb
+++ app/models/concerns/periodic.rb
@@ -1,35 +1,47 @@
 module Periodic
   extend ActiveSupport::Concern
-  
+
+  def self.begins
+    [
+      ["Today", Time.zone.now.beginning_of_day, 0],
+      ["Yesterday", Time.zone.now.yesterday.beginning_of_day, 1],
+      ["Earlier this Week", Time.zone.now.beginning_of_week, 2],
+      ["Last Week", Time.zone.now.beginning_of_week - 1.week, 3],
+      ["Two Weeks Ago", Time.zone.now.beginning_of_week - 2.weeks, 4],
+      ["Earlier this Month", Time.zone.now.beginning_of_month, 5],
+      ["Earlier this Month", Time.zone.now.beginning_of_month, 5]
+    ]
+  end
+
   included do
     scope :by_period, -> {
       group_by(&:period).each { yield }
       self
     }
   end
-  
+
+  def period_search_range
+    return "uploaded>#{period_begin.to_date},uploaded<#{period_end.to_date}"
+  end
+
+  def period_begin
+    period_pair{ ["", self.created_at.beginning_of_month, -1] }[1]
+  end
+
+  def period_end
+    pair = period_pair { ["", self.created_at.end_of_month + 1.day, -1] }
+    return Periodic.begins[pair[2] - 1][1] if pair[2] > 0
+    return Time.zone.now if pair[2] == 0
+    pair[1]
+  end
+
+  def period_pair
+    matches = Periodic.begins.filter{|a| self.created_at > a[1]}
+    return matches[0] if matches.length > 0
+    yield
+  end
+
   def period
-    return "Today" if self.created_at > Time.zone.now.beginning_of_day
-    if self.created_at > Time.zone.now.yesterday.beginning_of_day
-      return "Yesterday"
-    end
-
-    if self.created_at > Time.zone.now.beginning_of_week
-      return "Earlier this Week"
-    end
-
-    if self.created_at > (Time.zone.now.beginning_of_week - 1.week)
-      return "Last Week"
-    end
-
-    if self.created_at > (Time.zone.now.beginning_of_week - 2.weeks)
-      return "Two Weeks Ago"
-    end
-
-    if self.created_at > Time.zone.now.beginning_of_month
-      return "Earlier this Month"
-    end
-
-    self.created_at.strftime('%B %Y')
+    period_pair{ [self.created_at.strftime('%B %Y'), self.created_at, -1] }[0]
   end
 end
