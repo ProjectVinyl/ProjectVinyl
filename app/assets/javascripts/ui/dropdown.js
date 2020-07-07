43ca12acbe4608424e0ff89fae09f8de89ef1cfb
Sollace <sollacea@gmail.com>
2020-07-07 22:11:08 +0200

    Improve dropdown fitting to narrow screens

--- app/assets/javascripts/ui/dropdown.js
+++ app/assets/javascripts/ui/dropdown.js
@@ -4,20 +4,31 @@ import { addDelegatedEvent, halt, bindEvent } from '../jslim/events';
 addDelegatedEvent(document, 'click', '.popper .pop-out-toggle, .popper.pop-out-toggle', (e, target) => {
   if (e.which != 1) return;
   e.preventDefault();
-  
+
   target = target.closest('.popper');
   target.classList.toggle('pop-out-shown');
   if (!target.classList.contains('pop-out-shown')) return;
-  
+
   const content = target.querySelector('.pop-out');
-  
-  target.classList.toggle('pop-left', false);
-  target.classList.toggle('pop-right', false);
-  
+
+  target.classList.remove('pop-left', 'pop-right', 'pop-down');
+
   requestAnimationFrame(() => {
     const left = offset(content).left;
-    target.classList.toggle('pop-left', left + content.offsetWidth > document.documentElement.offsetWidth);
-    target.classList.toggle('pop-right', left < 0);
+    const hitRight = left + content.offsetWidth > document.documentElement.offsetWidth;
+    const hitLeft = left < 0;
+
+    const parent = content.parentNode;
+    const parentLeft = offset(content.parentNode).left;
+
+    const fallRightWouldOutFlow = parentLeft + content.offsetWidth > document.documentElement.offsetWidth;
+    const fallLeftWouldOutFlow = (parentLeft + parent.offsetWidth - content.offsetWidth) < 0;
+
+    if (hitRight || hitLeft) {
+      target.classList.toggle('pop-left', hitRight && !fallRightWouldOutFlow);
+      target.classList.toggle('pop-right', hitLeft && !fallLeftWouldOutFlow);
+      target.classList.toggle('pop-down', fallLeftWouldOutFlow && fallRightWouldOutFlow);
+    }
   });
 });
 
@@ -28,19 +39,19 @@ bindEvent(document, 'mouseup', () => all('.pop-out-shown', a => {
 addDelegatedEvent(document, 'touchstart', '.drop-down-holder:not(.hover), .mobile-touch-toggle:not(.hover)', (e, target) => {
   e.preventDefault();
   target.classList.add('hover');
-  
+
   // ffs https://www.chromestatus.com/features/5093566007214080
   ['touchstart', 'touchmove'].forEach(t => {
     target.addEventListener(t, clos, { passive: false });
     document.addEventListener(t, clos, { passive: false });
   });
-  
+
   function clos(e) {
     if (e.target.closest('a')) return; // Links should only be triggered on touch when the drop-down is already open
     halt(e);
-    
+
     target.classList.remove('hover');
-    
+
     ['touchstart', 'touchmove'].forEach(t => {
       target.removeEventListener(t, clos);
       document.removeEventListener(t, clos);
