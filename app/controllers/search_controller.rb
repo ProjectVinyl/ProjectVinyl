579f4a6d219e496ff7ea17e27dd042bcfd69fbc6
Sollace <sollacea@gmail.com>
2020-07-24 16:48:12 +0200

    Clean up and fix the random video function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,11 @@ class SearchController < ApplicationController
   def index
     read_params(params)
 
-    @results = ProjectVinyl::ElasticSearch::ElasticSelector.new(current_user, @query)
-    @results.videos
+    @results = ProjectVinyl::ElasticSearch::ElasticSelector.new(current_user, @query).videos
     @results.order(session, @orderby, @ascending)
     
     if @randomize && @single = @results.randomized(1).exec.records.first
-      return redirect_to action: :show, controller: @type_sym, id: @single.id
+      return redirect_to action: :show, controller: :videos, id: @single.id
     end
     
     @results = @results.query(@page, 20).exec
@@ -18,10 +17,8 @@ class SearchController < ApplicationController
     if params[:format] == 'json'
       return render_pagination_json @partial, @results
     end
-    
-    @type_label = 'Video'
+
     @data = URI.encode_www_form(
-      type: @type,
       order: params[:order],
       orderby: @orderby,
       query: @query
@@ -29,39 +26,22 @@ class SearchController < ApplicationController
     @crumb = {
       stack: [
         {
-          title: "Search"
+          title: 'Videos',
+          link: '/videos'
         }
       ],
-      title: @type_label.pluralize
+      title: 'Search'
     }
   end
   
   private
   def read_params(params)
-    @type = params[:type].to_i
     @partial = partial_for_type(:videos)
     
     @page = params[:page].to_i
     @ascending = (params[:order] || 0).to_i == 0
     @orderby = params[:orderby].to_i
     @randomize = params[:format] != 'json' && params[:random] == 'y'
-    
-    @title_query = params[:tq] || ''
-    @tag_query = params[:q] || ''
-    
-    if params[:format] == 'json'
-      @query = params[:tq] || params[:q] || ""
-    else
-      @query = ""
-      if !@title_query.empty?
-        @query << "title:#{@title_query}"
-      end
-      if !@tag_query.empty? && !@query.empty?
-        @query << ","
-      end
-      @query << @tag_query
-    end
-
-    @results = []
+    @query = params[:q] || ""
   end
 end
