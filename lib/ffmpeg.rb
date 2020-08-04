9c7cbac2a51250f1d0099b3483e7434048619766
Sollace <sollacea@gmail.com>
2020-08-04 20:34:28 +0200

    Forgot this

--- lib/ffmpeg.rb
+++ lib/ffmpeg.rb
@@ -5,7 +5,7 @@ class Ffmpeg
   def self.escape_filter_par(par)
     par.gsub(/([\(\),])/, '\\\\\1')
   end
-  
+
   HEADER = ['ffmpeg', '-hide_banner', '-nostats', '-loglevel', 'panic', '-y'].freeze
   SQUARE_CROP_COM = escape_filter_par('crop=min(iw,ih):min(iw,ih)').freeze
   SQUARE_TWO_FORTY = escape_filter_par('scale=min(min(iw,ih),240):min(min(iw,ih),240)').freeze
@@ -19,19 +19,23 @@ class Ffmpeg
   def self.compute_checksum(data)
     Digest::MD5.hexdigest(data)
   end
-  
+
   def self.get_video_length(file)
     get_attribute(file, "duration")
   end
-  
+
   def self.get_video_width(file)
     get_attribute(file, "width")
   end
-  
+
   def self.get_video_height(file)
     get_attribute(file, "height")
   end
-  
+
+  def self.get_dimensions(file)
+    [ get_video_width(file), get_video_height(file) ]
+  end
+
   def self.get_attribute(file, attribute)
     output = probe("stream", file, attribute)
     if output == 0
@@ -40,7 +44,7 @@ class Ffmpeg
 
     output.floor
   end
-  
+
   def self.cycle_lock(file, unlock)
     webm = file.to_s.split('.')[0] + '.webm'
     temp = Rails.root.join('encoding', File.basename(webm).to_s).to_s
@@ -51,19 +55,19 @@ class Ffmpeg
     end
     false
   end
-  
+
   def self.locked?(file)
     cycle_lock(file, false) do
       yield if block_given?
     end
   end
-  
+
   def self.try_unlock?(file)
     cycle_lock(file, true) do
       yield if block_given?
     end
   end
-  
+
   def self.encode_file(record, input, output, ext)
     FileUtils.mkdir_p(File.dirname(output))
 
@@ -107,7 +111,7 @@ class Ffmpeg
     end
     "Started"
   end
-  
+
   def self.to_h_m_s(length)
     h = length / 3600
     m = (length / 60) % 60
@@ -131,35 +135,35 @@ class Ffmpeg
     hms.unshift 0 while hms.length < 3
     (hms[0] * 3600) + (hms[1] * 60) + hms[2]
   end
-  
+
   def self.crop_avatar(src, dst)
     run_command('-i', src, '-vf', "#{SQUARE_CROP_COM},#{SQUARE_TWO_FORTY}", dst)
   end
-  
+
   def self.crop_square(src, dst)
     run_command('-i', src, '-vf', SQUARE_CROP_COM, dst)
   end
-  
+
   def self.scale(src, dst, *args)
     args << args[0] if args.length == 1
     run_command('-i', src, '-vf', escape_filter_par("scale=#{args[0]}:#{args[1]}"), dst)
   end
-  
+
   def self.extract_thumbnail(src, dst_full, dst_sml, time)
     time = to_h_m_s_accurate(time)
     run_command('-i', src, '-ss', time, '-vframes', 1, dst_full, '-ss', time, '-vframes', 1, '-vf', SCALE_ONE_THIRTY, dst_sml)
   end
-  
+
   def self.extract_tiny_thumb_from_existing(src, dst)
     run_command('-i', src, '-vf', SCALE_ONE_THIRTY, dst)
   end
-  
+
   private
   def self.probe(variant, file, field)
     stdout, error_str, status = Open3.capture3('ffprobe', '-v', 'error', '-show_entries', "#{variant}=#{field}", '-of', 'default=noprint_wrappers=1:nokey=1', file.to_s)
     stdout.to_i
   end
-  
+
   def self.run_command(*com)
     com = HEADER + com.map{|i| i.to_s}
     puts "FFMPEG RUN: #{com}"
