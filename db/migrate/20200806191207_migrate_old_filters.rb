e54d5be5fd82d4d36d04f0787252655df87c33e9
Sollace <sollacea@gmail.com>
2020-08-06 21:38:24 +0200

    Try again with this upgrade

--- db/migrate/20200806191207_migrate_old_filters.rb
+++ db/migrate/20200806191207_migrate_old_filters.rb
@@ -3,7 +3,9 @@ class MigrateOldFilters < ActiveRecord::Migration[5.1]
     User.all.each do |user|
       user_id = user.id
       tags = TagSubscription.where('user_id = ? AND (hide = true OR spoiler = true)', user_id).pluck(:tag_id, :hide, :spoiler)
-      
+
+      SiteFilter.reset_column_information
+
       if tags.length > 0
         hide = Tag.where('id IN (?)', tags.filter{|t| t[1]}.map{|t| t[0]}).actual_names.join(',')
         spoiler = Tag.where('id IN (?)', tags.filter{|t| t[2]}.map{|t| t[0]}).actual_names.join(',')
