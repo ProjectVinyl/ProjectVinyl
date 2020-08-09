42bae2ab82b9bcc3585ff94dce1f80f6d47c3a36
Sollace <sollacea@gmail.com>
2020-08-09 16:04:34 +0200

    Only display the hide/spoiler options when the user can modify the current filter

--- app/helpers/filters_helper.rb
+++ app/helpers/filters_helper.rb
@@ -3,7 +3,7 @@ module FiltersHelper
   def default_filter
     @default_filter ||= SiteFilter.where(user_id: nil, preferred: true).first || SiteFilter.new
   end
-  
+
   def session_filter
     @session_filter ||= SiteFilter.where(id: cookies[:filter], user_id: nil).first if cookies[:filter]
     @session_filter || default_filter
@@ -11,5 +11,9 @@ module FiltersHelper
 
   def current_filter
     @current_filter ||= user_signed_in? && current_user.site_filter ? current_user.site_filter : session_filter
+  end
+
+  def can_modify_filter?
+    user_signed_in? && current_filter.user_id == current_user.id
   end
 end
