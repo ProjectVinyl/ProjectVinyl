b1f3b3cfdf9b960e2878dae775a3d3cf3901f2c9
Sollace <sollacea@gmail.com>
2020-08-12 21:31:40 +0200

    Fixed the feature toggle

--- app/controllers/admin/videos/featured_videos_controller.rb
+++ app/controllers/admin/videos/featured_videos_controller.rb
@@ -3,15 +3,20 @@ module Admin
     class FeaturedVideosController < BaseVideosAdminController
       def update
         check_then do |video|
-          Video.where(featured: true).update_all(featured: false)
-          render json: {
-            added: video.featured = !video.featured
-          }
-
-          if video.featured
-            video.save
-            video.add_tag('featured video')
+          Video.where(featured: true).each do |v|
+            v.featured = false
+            v.save
+            v.update_index(defer: true)
           end
+
+          video.featured = !video.featured
+          video.add_tag('featured video') if video.featured
+          video.save
+          video.update_index(defer: true)
+
+          render json: {
+            added: video.featured
+          }
         end
       end
     end
