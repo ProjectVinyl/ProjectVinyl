1a576d05f75494e97de4a9c3f73b10ace949e5d5
Sollace <sollacea@gmail.com>
2020-08-14 01:00:22 +0200

    Some more work on the home page, added a section for "active" videos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,26 +4,32 @@ class WelcomeController < ApplicationController
     @threads = Comment.with_threads("Board").order(:created_at).reverse_order.limit(5)
     @featured_album = Album.where('featured > 0').first
 
-    @all = current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
+    @all = cached_videos(current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
               .order(:created_at)
               .reverse_order
-              .limit(90)
-              .records
-              .for_thumbnails(current_user)
+              .limit(90), 'welcome_all').for_thumbnails(current_user)
 
-    begin
-      mode = :painless
+    @active = cached_videos(current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
+              .order(:boosted)
+              .filter({
+                range: {
+                  updated_at: { gt: DateTime.now - 3.days }
+                }
+              })
+              .reverse_order
+              .limit(6), 'welcome_active').for_thumbnails(current_user)
 
-      if mode == :wilson
-        @popular = current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
-            .order(:wilson_lower_bound)
-            .reverse_order
-            .limit(4)
-            .records
-            .for_thumbnails(current_user)
-      else
-        popular_ids = cache("welcome_popular", expires_in: 1.minute) do
-          current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
+    mode = :painless
+
+    if mode == :wilson
+      @popular = current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
+          .order(:wilson_lower_bound)
+          .reverse_order
+          .limit(4)
+          .records
+          .for_thumbnails(current_user)
+    else
+      @popular = cached_videos(current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
                   .sort({
                     _script: {
                       type: :number,
@@ -37,19 +43,7 @@ class WelcomeController < ApplicationController
                       order: :desc
                     }
                   })
-                  .limit(4)
-                  .ids
-        end
-        @popular = Video.where('videos.id IN (?)', popular_ids).for_thumbnails(current_user)
-      end
-    rescue Elasticsearch::Transport::Transport::Errors::InternalServerError => e
-      @exception_flag = true
-      @popular = current_filter.videos.where(hidden: false, listing: 0, duplicate_id: 0)
-                .order(:heat, :updated_at, :created_at)
-                .reverse_order
-                .limit(4)
-                .records
-                .for_thumbnails(current_user)
+                  .limit(6), "welcome_popular").for_thumbnails(current_user)
     end
 
     @featured = current_filter.videos.where(hidden: false, duplicate_id: 0, featured: true)
@@ -58,5 +52,13 @@ class WelcomeController < ApplicationController
               .for_thumbnails(current_user)
               .first
     @feed = TagSubscription.get_feed_items(current_user, current_filter).limit(15) if user_signed_in?
+  end
+
+  def cached_videos(elastic_record, key)
+    ids = cache(key, expires_in: 1.minute) do
+      elastic_record.ids
+    end
+
+    Video.where(id: ids).order("position(videos.id::text in '#{ids.join(',')}')")
   end
 end
