f297fa951c87b356378920e716c0215a8ca2474f
Sollace <sollacea@gmail.com>
2020-08-15 18:07:24 +0200

    Store cookies correctly

--- app/assets/javascripts/utils/cookies.js
+++ app/assets/javascripts/utils/cookies.js
@@ -1,5 +1,8 @@
 import { tryUnmarshal } from './misc';
 
+const DAYS = 24 * 60 * 60 * 1000;
+const COOKIE_MAX_AGE = 30 * DAYS;
+
 function extractCookieValue(key) {
   let cook = document.cookie;
   let index = cook.lastIndexOf(key);
@@ -16,7 +19,9 @@ export const cookies = {
     if (key) return tryUnmarshal(decodeURIComponent(key));
   },
   set: (key, value) => {
-    if (value == null || value === undefined || isNaN(value)) value = '';
-    document.cookie = `${key}=${encodeURIComponent(value)};`;
+    if (value == null || value === undefined || isNaN(value)) {
+      return document.cookie = `${key}=; max-age=-1; path=/;`;
+    }
+    document.cookie = `${key}=${encodeURIComponent(value)}; max-age=${COOKIE_MAX_AGE}; path=/;`;
   }
 };
