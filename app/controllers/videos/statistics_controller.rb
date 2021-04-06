c092762396e007f2d2a6b2d7c65d41b61887de1e
Sollace <sollacea@gmail.com>
2021-04-06 21:00:53 +0200

    Added breadcrumbs to the statistics page

--- app/controllers/videos/statistics_controller.rb
+++ app/controllers/videos/statistics_controller.rb
@@ -2,6 +2,14 @@ module Videos
   class StatisticsController < BaseVideosController
     def show
       head :not_found if !(@video = Video.where(id: params[:video_id]).first)
+
+      @crumb = {
+        stack: [
+          { title: 'Videos' },
+          { link: @video.link, title: "##{@video.id}" }
+        ],
+        title: 'Statistics'
+      }
     end
   end
 end
