bed737f3ae20969431563a432f15fca4feeabe15
Sollace <sollacea@gmail.com>
2021-04-07 11:20:10 +0200

    Move code for reordering to a concern

--- app/models/album_item.rb
+++ app/models/album_item.rb
@@ -1,42 +1,23 @@
 class AlbumItem < ApplicationRecord
+  include Reorderable
+
   belongs_to :album
   belongs_to :video
   has_one :direct_user, through: :video
 
-  scope :following, ->(current) { discriminate('>', current) }
-  scope :leading, ->(current) { discriminate('<', current) }
-  scope :discriminate, ->(comparitor, current) { where('album_items.index ' + comparitor + ' ?', current) }
-  
-  before_destroy :shift_indices
-  
+  has_siblings :album_items
+  after_move :update_album_ordering
+
   def tiny_thumb(user, filter)
     self.video.tiny_thumb(user, filter)
   end
-  
+
   def owned_by(user)
     self.album.owned_by(user)
   end
-  
+
   def user
     self.direct_user || @dummy || (@dummy = User.dummy(self.video.user_id))
-  end
-  
-  def move(new_index)
-    from = self.index
-    to = new_index
-    if to != from
-      if to < from
-        self.update_indices(self.album.album_items.where('album_items.index >= ? AND album_items.index < ?', to, from), '+')
-      else
-        self.update_indices(self.album.album_items.where('album_items.index > ? AND album_items.index <= ?', from, to), '-')
-      end
-      if self.album.ordering && self.album.ordering > 0
-        self.album.ordering = 0
-        self.album.save
-      end
-      self.index = new_index
-      self.save
-    end
   end
 
   def link
@@ -59,12 +40,14 @@ class AlbumItem < ApplicationRecord
     video.hidden ? "hidden" : video.title
   end
 
-  protected
-  def shift_indices
-    self.update_indices(self.album.album_items.where('album_items.index > ?', self.index), '-')
+  private
+  def album_items
+    album.album_items
   end
-  
-  def update_indices(items, op)
-    items.update_all("index = index #{op} 1")
+  def update_album_ordering
+    if self.album.ordering && self.album.ordering > 0
+      self.album.ordering = 0
+      self.album.save
+    end
   end
 end
---
+++ app/models/concerns/reorderable.rb
@@ -0,0 +1,49 @@
+module Reorderable
+  extend ActiveSupport::Concern
+
+  PREV = -1
+  NEXT = 1
+
+  included do
+    before_destroy :shift_on_destroyed
+
+    scope :shift_by, ->(amount) { update_all("index = index + #{amount.to_i}") }
+    scope :following, ->(index) { discriminate('>', index) }
+    scope :leading, ->(index) { discriminate('<', index) }
+    scope :discriminate, ->(comparitor, index) { where('index ' + comparitor + ' ?', index) }
+
+    scope :shift_between, ->(from, to) {
+      return where('index >= ? AND index < ?', to, from).shift_by(NEXT) if to < from
+      where('index > ? AND index <= ?', from, to).shift_by(PREV)
+    }
+
+    def self.has_siblings(sym)
+      define_method :siblings do
+        self.send(sym)
+      end
+    end
+
+    def self.after_move(sym)
+      define_method :after_move do
+        self.send(sym)
+      end
+    end
+  end
+
+  def move(new_index)
+    from = index
+    to = new_index
+
+    if to != from
+      siblings.shift_between(from, to) if respond_to?(:siblings)
+      after_move if respond_to?(:after_move)
+      self.index = new_index
+      self.save
+    end
+  end
+
+  protected
+  def shift_on_destroyed
+    siblings.following.shift_by(PREV) if respond_to?(:siblings)
+  end
+end
