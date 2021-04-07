0a9760c7fef4e9d633f3ccf2c9df88f421b5fdba
Sollace <sollacea@gmail.com>
2021-04-07 11:14:10 +0200

    Add an away status to online indicators

--- app/models/concerns/activitied.rb
+++ app/models/concerns/activitied.rb
@@ -1,6 +1,6 @@
 module Activitied
   extend ActiveSupport::Concern
-  
+
   included do
     scope :online, -> { where('last_active_at > ?', Time.zone.now - 3.minutes) }
     scope :active_since, ->since {
@@ -8,15 +8,25 @@ module Activitied
       .order('last_active_at IS NOT NULL', :last_active_at, :updated_at)
     }
   end
-  
+
   def poke
     return if online?
     touch(:last_active_at)
     return if new_record?
     save
   end
-  
+
   def online?
     last_active_at != nil && last_active_at > Time.zone.now - 3.minutes
+  end
+
+  def away?
+    last_active_at != nil && last_active_at > Time.zone.now - 15.minutes
+  end
+
+  def online_status
+    return :online if online?
+    return :away if away?
+    :offline
   end
 end
