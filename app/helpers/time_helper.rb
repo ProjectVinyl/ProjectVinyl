68a5daa58e0927ce2fa6b0341196c237ead1cc26
Sollace <sollacea@gmail.com>
2021-04-16 19:31:12 +0200

    Move premier/upload time to a tooltip

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -3,14 +3,18 @@ module TimeHelper
   def since(date)
     "#{time_ago_in_words localise date} ago"
   end
-  
+
   def current_time_zone
     return current_user.time_zone if current_user
     Time.zone
   end
-  
+
   def localise(date)
     return date if date.nil?
     date.in_time_zone current_time_zone
+  end
+
+  def localise_ftime(date)
+    localise(date).to_time.strftime('%e %B %Y at %H:%m:%S')
   end
 end
