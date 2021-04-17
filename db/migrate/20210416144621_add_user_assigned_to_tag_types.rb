b0cc4b3f032cddca180d62d332ba86ce201e82b2
Sollace <sollacea@gmail.com>
2021-04-17 12:57:27 +0200

    Touch

--- db/migrate/20210416144621_add_user_assigned_to_tag_types.rb
+++ db/migrate/20210416144621_add_user_assigned_to_tag_types.rb
@@ -1,7 +1,7 @@
 class AddUserAssignedToTagTypes < ActiveRecord::Migration[5.1]
   def change
     add_column :tag_types, :user_assignable, :boolean, default: true
-    
+
     TagType.reset_column_information
     TagType.where(prefix: [:rating, :warning]).update_all(user_assignable: false)
   end
