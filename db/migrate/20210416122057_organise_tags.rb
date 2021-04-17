b36e28e3d3b238987c352b855d34d0c65184b906
Sollace <sollacea@gmail.com>
2021-04-17 12:59:59 +0200

    Touch

--- db/migrate/20210416122057_organise_tags.rb
+++ db/migrate/20210416122057_organise_tags.rb
@@ -5,7 +5,7 @@ class OrganiseTags < ActiveRecord::Migration[5.1]
     add_column :tags, :namespace, :string, default: ''
     add_column :tags, :suffex, :string, default: ''
     add_column :tags, :slug, :string, default: ''
-    
+
     Tag.reset_column_information
     Tag.where("name LIKE '%:%'")
        .update_all("slug = replace(name, ':', '-colon-'), namespace = split_part(name, ':', 1), suffex = replace(name, concat(split_part(name, ':', 1), ':'), '')")
