0dda5c9967d292bba9f5ca2cd64224673353b1af
Sollace <sollacea@gmail.com>
2021-06-28 21:25:14 +0200

    Album thumbnails now contain a "play all" link

--- app/models/albums/album_like.rb
+++ app/models/albums/album_like.rb
@@ -5,5 +5,11 @@ module Albums
       extend Forwardable
       def_delegator :video_set, :virtual?
     end
+
+    def play_all_path
+      vid_id = album_items.order(:index).limit(1).pluck(:video_id).first
+      return nil if vid_id.nil?
+      "/#{vid_id}?list=#{id}&index=0"
+    end
   end
 end
