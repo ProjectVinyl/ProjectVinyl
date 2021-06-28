353be8f848deff9f2d602af23e70ce47e118ed2d
Sollace <sollacea@gmail.com>
2021-06-28 18:13:14 +0200

    Removed js splitting and joining of rows

--- app/assets/javascripts/ui/grid/all.js
+++ app/assets/javascripts/ui/grid/all.js
@@ -1,21 +1,7 @@
 import { ready, bindEvent } from '../../jslim/events';
 import { alignLists } from './row';
-import { resizeGrid } from './split';
 
 ready(() => {
-  const columnRight = document.querySelector('.grid-root.column-right');
-  if (!columnRight) {
-    alignLists();
-    return bindEvent(window, 'resize', alignLists);
-  }
-  
-  const columnLeft = document.querySelector('.grid-root.column-left');
-  
-  function resize() {
-    alignLists();
-    resizeGrid(columnLeft, columnRight);
-  }
-  
-  bindEvent(window, 'resize', resize);
-  resize();
+  alignLists();
+  return bindEvent(window, 'resize', alignLists);
 });
