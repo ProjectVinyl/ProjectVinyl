93301f13fd74cf5bc00116eca493e194ebddf43a
Sollace <sollacea@gmail.com>
2021-07-01 17:59:23 +0200

    Fixed poppers not falling left when you're meant to

--- app/assets/javascripts/ui/dropdown.js
+++ app/assets/javascripts/ui/dropdown.js
@@ -1,18 +1,25 @@
 import { all, offset } from '../jslim/dom';
 import { addDelegatedEvent, halt, bindEvent } from '../jslim/events';
 
-addDelegatedEvent(document, 'click', '.popper .pop-out-toggle, .popper.pop-out-toggle', (e, target) => {
-  if (e.which != 1) return;
-  e.preventDefault();
+function closeAllPoppers() {
+  all('.pop-out-shown', a => {
+    if (!a.querySelector('*:hover')) {
+      a.classList.remove('pop-out-shown');
+    }
+  });
+}
 
-  target = target.closest('.popper');
+function togglePopper(target) {
   target.classList.toggle('pop-out-shown');
-  if (!target.classList.contains('pop-out-shown')) return;
+  if (target.classList.contains('pop-out-shown')) {
+    adjustFloyoutDirection(target);
+  }
+}
 
+function adjustFloyoutDirection(target) {
   const content = target.querySelector('.pop-out');
 
   target.classList.remove('pop-left', 'pop-right', 'pop-center');
-
   requestAnimationFrame(() => {
     const left = offset(content).left;
     const hitRight = left + content.offsetWidth > document.documentElement.offsetWidth;
@@ -24,14 +31,20 @@ addDelegatedEvent(document, 'click', '.popper .pop-out-toggle, .popper.pop-out-t
     const fallRightWouldOutFlow = parentLeft + content.offsetWidth > document.documentElement.offsetWidth;
     const fallLeftWouldOutFlow = (parentLeft + parent.offsetWidth - content.offsetWidth) < 0;
 
+    console.log({ left, hitLeft, hitRight })
+
     if (hitRight || hitLeft) {
-      target.classList.toggle('pop-left', hitRight && !fallRightWouldOutFlow);
-      target.classList.toggle('pop-right', hitLeft && !fallLeftWouldOutFlow);
+      target.classList.toggle('pop-left', hitRight && !fallLeftWouldOutFlow);
+      target.classList.toggle('pop-right', hitLeft && !fallRightWouldOutFlow);
       target.classList.toggle('pop-center', fallLeftWouldOutFlow && fallRightWouldOutFlow);
     }
   });
+}
+
+addDelegatedEvent(document, 'click', '.popper .pop-out-toggle, .popper.pop-out-toggle', (e, target) => {
+  if (e.which != 1) return;
+  e.preventDefault();
+  togglePopper(target.closest('.popper'));
 });
 
-bindEvent(document, 'mouseup', () => all('.pop-out-shown', a => {
-  if (!a.querySelector('.action.toggle:hover')) a.classList.remove('pop-out-shown');
-}));
+bindEvent(document, 'mouseup', closeAllPoppers);
