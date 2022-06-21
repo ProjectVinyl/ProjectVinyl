43ee5f7ca96dbc9bab335b42abec3b9a9b007f1b
Sollace <sollacea@gmail.com>
2022-06-21 18:39:01 +0200

    Fix error when settings.yml does not exist

--- config/initializers/application_settings.rb
+++ config/initializers/application_settings.rb
@@ -2,22 +2,23 @@ require 'yaml'
 
 module ApplicationSettings
   PATH = Rails.root.join('config', 'settings.yml')
-  
+
   def self.load
-    YAML::load_file(PATH) || {}
+    return YAML::load_file(PATH) || {} if File.exist?(PATH)
+    {}
   end
-  
+
   def self.get(key)
     ApplicationSettings.load[key]
   end
-  
+
   def self.set(key, value)
     d = ApplicationSettings.load
     d[key] = value
     File.write(PATH, d.to_yaml)
     value
   end
-  
+
   def self.toggle(key)
     d = ApplicationSettings.load
     d[key] = !d[key]
