ad8b158783809191a7e5b8c918cbde902bc0243c
Sollace <sollacea@gmail.com>
2022-06-21 17:34:32 +0200

    Switch to yt-dlp

--- lib/youtubedl.rb
+++ lib/youtubedl.rb
@@ -3,7 +3,7 @@ require 'open3'
 
 class Youtubedl
   def self.video_meta(src)
-    stdout, error_str, status = Open3.capture3('youtube-dl', src, '--dump-json')
+    stdout, error_str, status = Open3.capture3('yt-dlp', src, '--dump-json')
     JSON.parse(stdout, symbolize_names: true)
   end
 end
