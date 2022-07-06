c9c55ab91c577f944c01adbd21ed2fe8757ae99d
Sollace <sollacea@gmail.com>
2022-07-06 20:52:56 +0200

    Use the helper for durations

--- app/helpers/formats_helper.rb
+++ app/helpers/formats_helper.rb
@@ -13,24 +13,15 @@ module FormatsHelper
       }
     })
   end
-  
-  def duration(length)
-    if length == 0
-      return '--:--'
-    end
-    
-    Ffmpeg.to_h_m_s(length) do |h, m, s|
-      if h == 0
-        return format("%02d:%02d", m, s)
-      end
-    end
+
+  def duration(length, allow_zero: false)
+    return '--:--' if !allow_zero && length == 0
+    Ffmpeg.to_h_m_s(length, cut_leading_zero_hours: true)
   end
-  
+
   def safe_to_display(num, max)
     num ||= 0
-    if num > max
-      return number_with_delimiter(max) + '+'
-    end
+    return number_with_delimiter(max) + '+' if num > max
     number_with_delimiter num
   end
 end
