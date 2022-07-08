4bdf01d9a77c036e5dcd142d8b5647e0d9d3ecb8
Sollace <sollacea@gmail.com>
2022-07-08 22:56:55 +0200

    Fix some broken thumbnails

--- app/controllers/assets/videos/thumbs_controller.rb
+++ app/controllers/assets/videos/thumbs_controller.rb
@@ -3,8 +3,8 @@ module Assets
     class ThumbsController < BaseAssetsController
       def show
         with_video do |video|
-          return redirect_to '/images/default-cover-small.svg' if !(video && video.visible_to?(current_user))
-          return redirect_to '/images/default-cover-small-g.svg' if !special_access?
+          return redirect_to '/images/default-cover.svg' if !(video && video.visible_to?(current_user))
+          return redirect_to '/images/default-cover-g.svg' if !special_access?
           return serve_direct video.tiny_cover_path, 'image/png', fallback: '/images/default-cover.svg'
         end
       end
