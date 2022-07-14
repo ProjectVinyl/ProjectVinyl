875bccb0825538ed5c5c8c87b4bcd66d64f8c4d8
Sollace <sollacea@gmail.com>
2022-07-14 22:29:32 +0200

    Rewrites and context menu should now update when values change

--- app/assets/javascripts/components/videos/context_menu.js
+++ app/assets/javascripts/components/videos/context_menu.js
@@ -1,6 +1,8 @@
 import { cookies } from '../../utils/cookies';
 
-const speeds = [
+const DEFAULT_SPEED = 2;
+const DEFAULT_INCREMENTER = (on => !on);
+const SPEEDS = [
   {name: '0.25x', value: 0.25},
   {name: '0.5x', value: 0.5},
   {name: 'Normal', value: 1},
@@ -10,66 +12,50 @@ const speeds = [
 ];
 
 export function initContextMenu(el, player) {
-  const actions = {
-    setAutostart(on) {
-      player.__autostart = on;
-      if (!player.nonpersistent) {
-        cookies.set('autostart', on);
-      }
-      return on;
-    },
-    setAutoplay(on) {
-      player.__autoplay = on;
-      if (!player.nonpersistent) {
-        cookies.set('autoplay', on);
-      }
-      if (on) {
-        actions.setLoop(false);
-      }
+  const actions = {};
+  const menuItems = {};
 
-      return on;
-    },
-    setLoop(on) {
-      player.__loop = on;
-
-      if (player.video) {
-        player.video.loop = on;
+  function createSetter(name, label, cookieName, def, applicator, incrementer) {
+    menuItems[label] = {
+      getter() {
+        return player['__' + name.toLowerCase()];
+      },
+      incrementer: (incrementer || DEFAULT_INCREMENTER),
+      setter(value) {
+        player['__' + name.toLowerCase()] = value;
+        if (!player.nonpersistent) {
+          cookies.set(cookieName, value);
+        }
+        if (applicator) {
+          value = applicator(value);
+        }
+        if (this.onChange) {
+          this.onChange(value);
+        }
       }
+    };
+    actions['set' + name] = value => menuItems[label].setter(value);
+    menuItems[label].setter(cookies.get(cookieName, def === undefined ? false : def));
+  }
 
-      return on;
-    },
-    setSpeed(speed) {
-      player.__speed = speed % speeds.length;
-      speed = speeds[player.__speed];
-
-      if (player.video) {
-        player.video.playbackRate = speed.value;
-      }
-
-      return speed.name;
+  createSetter('Loop', 'Loop', 'loopplayback', false, on => {
+    if (player.video) {
+      player.video.loop = on;
     }
-  };
-  const menuItems = {
-    'Loop': {
-      initial: actions.setLoop(false),
-      callback: () => actions.setLoop(!player.__loop)
-    },
-    'Speed': {
-      initial: actions.setSpeed(2),
-      callback: () => actions.setSpeed(player.__speed + 1)
-    },
-    'Auto Play': {
-      initial: actions.setAutostart(!!cookies.get('autostart')),
-      callback: () => actions.setAutostart(!player.__autostart)
-    },
-    'Auto Next': {
-      initial: actions.setAutoplay(!!cookies.get('autoplay')),
-      display: player.params.autoplay,
-      callback: ()  => actions.setAutoplay(!player.__autoplay)
+    return on;
+  });
+  createSetter('Speed', 'Speed', 'playbackspeed', DEFAULT_SPEED, speed => {
+    player.__speed %= SPEEDS.length;
+    speed = SPEEDS[player.__speed];
+    if (player.video) {
+      player.video.playbackRate = speed.value;
     }
-  };
+    return speed.name;
+  }, on => on + 1);
+  createSetter('Autostart', 'Auto Play', 'autostart');
+  createSetter('Autoplay', 'Auto Next', 'autoplay');
+
   player.dom.addEventListener('contextmenu:shown', ev => {
-    console.log(ev);
     ev.detail.data.buildMenu(menuItems);
   });
 
--- app/assets/javascripts/ui/contextmenu.js
+++ app/assets/javascripts/ui/contextmenu.js
@@ -1,44 +1,65 @@
 import { offset } from '../jslim/dom';
 import { addDelegatedEvent, bindEvent, halt, dispatchEvent } from '../jslim/events';
 
-function buildMenu(dom, items) {
-  const keys = Object.keys(items);
 
-  keys.forEach(key => {
-    const label = dom.querySelector(`[data-option="${key}"] .value`);
-    if (label) {
-      items[key].initial(newValue => {
-        label.innerHTML = s === true ? '<i class="fa fa-check"></i>' : (s || '');
-      });
-    }
-  });
-  
-  dom.querySelectorAll('[data-option]').forEach(option => {
-    const item = items[option.dataset.option];
-    option.dataset.value = item ? item.initial : '';
-  });
+addDelegatedEvent(document, 'contextmenu', '.context-menu-parent:not([data-nocontext="true"])', (ev, sender) => {
+  const dom = sender.querySelector('.contextmenu');
+  const x = ev.clientX, y = ev.clientY;
 
-  addDelegatedEvent(dom, 'click', '[data-option]', (e, target) => {
-    const item = items[target.dataset.option];
-    if (item) {
-      target.dataset.value = item.callback();
-    }
-    halt(e);
-  });
-}
+  ev.preventDefault();
+
+  if (dom.dataset.initialized !== 'true') {
+    dom.dataset.initialized = true;
+    addDelegatedEvent(dom, 'click', '[data-option]', (e, target) => {
+      const item = dom.items ? dom.items[target.dataset.option] : null;
+      if (item) {
+        item.onChange = value => {
+          target.dataset.value = value;
+        };
+        item.setter(item.incrementer(item.getter()));
+      }
+      halt(e);
+    });
+
+    dispatchEvent('contextmenu:shown', {
+      buildMenu(items) {
+        dom.items = items;
+        performShow(x, y, sender, dom);
+      }
+    }, dom);
+  } else {
+    performShow(x, y, sender, dom);
+  }
+});
+
+bindEvent(window, 'resize', hideAll);
+bindEvent(window, 'blur', hideAll);
+bindEvent(document, 'click', ev => {
+  if (ev.button === 0) hideAll();
+});
 
 function performShow(x, y, container, dom) {
+  dom.querySelectorAll('[data-option]').forEach(option => {
+    const item = dom.items ? dom.items[option.dataset.option] : null;
+    if (item) {
+      item.onChange = value => {
+        option.dataset.value = value;
+      };
+      item.setter(item.getter());
+    }
+  });
+
   if (x + dom.offsetWidth >= document.body.clientWidth) {
     x = document.body.clientWidth - dom.offsetWidth;
   }
   if (y + dom.offsetHeight >= document.body.clientHeight) {
     y = document.body.clientHeight - dom.offsetHeight;
   }
-  
+
   const off = offset(container);
   x += document.scrollingElement.scrollLeft - off.left;
   y += document.scrollingElement.scrollTop - off.top;
-  
+
   dom.style.top = `${y}px`;
   dom.style.left = `${x}px`;
   dom.classList.remove('hidden');
@@ -60,32 +81,6 @@ function getFader(dom) {
   return fader;
 }
 
-export function hideAll() {
+function hideAll() {
   document.querySelectorAll('.contextmenu, .contextmenu-fader').forEach(p => p.classList.add('hidden'));
 }
-
-addDelegatedEvent(document, 'contextmenu', '.context-menu-parent:not([data-nocontext="true"])', (ev, sender) => {
-  const dom = sender.querySelector('.contextmenu');
-  const x = ev.clientX;
-  const y = ev.clientY;
-
-  ev.preventDefault();
-
-  if (dom.dataset.initialized !== 'true') {
-    dom.dataset.initialized = true;
-    dispatchEvent('contextmenu:shown', {
-      buildMenu(items) {
-        buildMenu(dom, items);
-        performShow(x, y, sender, dom);
-      }
-    }, dom);
-  } else {
-    performShow(x, y, sender, dom);
-  }
-});
-
-bindEvent(window, 'resize', hideAll);
-bindEvent(window, 'blur', hideAll);
-bindEvent(document, 'click', ev => {
-  if (ev.button === 0) hideAll();
-});
