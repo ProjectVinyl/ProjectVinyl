981e97c881bfde08a34140b688a9384bc9c98756
Sollace <sollacea@gmail.com>
2022-07-15 18:55:18 +0200

    We don't neeeeed this fader hack any more! :I

--- app/assets/javascripts/ui/contextmenu.js
+++ app/assets/javascripts/ui/contextmenu.js
@@ -1,6 +1,36 @@
 import { offset } from '../jslim/dom';
 import { addDelegatedEvent, bindEvent, halt, dispatchEvent } from '../jslim/events';
 
+function performShow(x, y, container, dom) {
+  dom.querySelectorAll('[data-option]').forEach(option => {
+    const item = dom.items ? dom.items[option.dataset.option] : null;
+    if (item) {
+      item.onChange = value => {
+        option.dataset.value = value;
+      };
+      item.setter(item.getter());
+    }
+  });
+
+  if (x + dom.offsetWidth >= document.body.clientWidth) {
+    x = document.body.clientWidth - dom.offsetWidth;
+  }
+  if (y + dom.offsetHeight >= document.body.clientHeight) {
+    y = document.body.clientHeight - dom.offsetHeight;
+  }
+
+  const off = offset(container);
+  x += document.scrollingElement.scrollLeft - off.left;
+  y += document.scrollingElement.scrollTop - off.top;
+
+  dom.style.top = `${y}px`;
+  dom.style.left = `${x}px`;
+  dom.classList.remove('hidden');
+}
+
+function hideAll() {
+  document.querySelectorAll('.contextmenu').forEach(p => p.classList.add('hidden'));
+}
 
 addDelegatedEvent(document, 'contextmenu', '.context-menu-parent:not([data-nocontext="true"])', (ev, sender) => {
   const dom = sender.querySelector('.contextmenu');
@@ -32,56 +62,13 @@ addDelegatedEvent(document, 'contextmenu', '.context-menu-parent:not([data-nocon
     performShow(x, y, sender, dom);
   }
 });
-
 bindEvent(window, 'resize', hideAll);
 bindEvent(window, 'blur', hideAll);
 bindEvent(document, 'click', ev => {
   if (ev.button === 0) hideAll();
 });
-
-function performShow(x, y, container, dom) {
-  dom.querySelectorAll('[data-option]').forEach(option => {
-    const item = dom.items ? dom.items[option.dataset.option] : null;
-    if (item) {
-      item.onChange = value => {
-        option.dataset.value = value;
-      };
-      item.setter(item.getter());
-    }
-  });
-
-  if (x + dom.offsetWidth >= document.body.clientWidth) {
-    x = document.body.clientWidth - dom.offsetWidth;
+window.addEventListener('click', ev => {
+  if (ev.button === 0 && document.querySelector('.contextmenu:not(.hidden)')) {
+    ev.preventDefault();
   }
-  if (y + dom.offsetHeight >= document.body.clientHeight) {
-    y = document.body.clientHeight - dom.offsetHeight;
-  }
-
-  const off = offset(container);
-  x += document.scrollingElement.scrollLeft - off.left;
-  y += document.scrollingElement.scrollTop - off.top;
-
-  dom.style.top = `${y}px`;
-  dom.style.left = `${x}px`;
-  dom.classList.remove('hidden');
-  getFader(dom).classList.remove('hidden');
-}
-
-function getFader(dom) {
-  let fader = dom.parentNode.querySelector('.contextmenu-fader');
-  if (!fader) {
-    dom.insertAdjacentHTML('beforebegin', '<div class="contextmenu-fader hidden"></div>');
-    fader = dom.parentNode.querySelector('.contextmenu-fader');
-    fader.addEventListener('click', ev => {
-      if (ev.which !== 1) return;
-      dom.classList.add('hidden');
-      fader.classList.add('hidden');
-      halt(ev);
-    });
-  }
-  return fader;
-}
-
-function hideAll() {
-  document.querySelectorAll('.contextmenu, .contextmenu-fader').forEach(p => p.classList.add('hidden'));
-}
+}, true);
