60da1e8c8f4859d34c7fefc8546219da8c659cb4
Sollace <sollacea@gmail.com>
2022-07-17 14:40:38 +0200

    Trim trailing whitespace

--- app/controllers/inbox/pm_controller.rb
+++ app/controllers/inbox/pm_controller.rb
@@ -1,7 +1,7 @@
 module Inbox
   class PmController < BaseInboxController
     before_action :check_requirements, only: [:destroy]
-    
+
     def show
       if !(user_signed_in? && @pm = Pm.find_for_user(params[:id], current_user))
         return render_error(
@@ -9,7 +9,7 @@ module Inbox
           description: "Either the thread does not exist or you don't have the necessary permissions to see it."
         )
       end
-      
+
       @order = params[:order].to_i
       @thread = @pm.comment_thread
       @comments = @thread.pagination(current_user, page: (params[:page] || -1).to_i, reverse: @order == 1)
@@ -24,13 +24,13 @@ module Inbox
         @pm.save
       end
     end
-    
+
     def new
       @thread = CommentThread.new
       @user = User.where(id: params[:user]).first if params[:user]
       render partial: 'new'
     end
-    
+
     def create
       return redirect_to action: :index, controller: :welcome if !user_signed_in?
 
@@ -44,10 +44,10 @@ module Inbox
     def destroy
       @order = params[:order].to_i
       @pm.toggle_deleted
-      
+
       @type = params[:type]
       @results = paginate_for_type(@type)
-      
+
       @json = pagination_json_for_render @results, partial: 'inbox/pm/thumb'
       @json[:tabs] = tab_changes
       render json: @json
