5ac9e4b3de7b30fb292c548b1526df96f5dec6e3
Sollace <sollacea@gmail.com>
2022-07-21 22:02:14 +0200

    More work on the story card editor. You can save and drag to re-order them in the list now.

--- app/controllers/videos/story_cards_controller.rb
+++ app/controllers/videos/story_cards_controller.rb
@@ -11,15 +11,21 @@ module Videos
 
     def update
       check_then do |video|
-        byebug
-        return head :unauthorized if !@video.owned_by(current_user)
+        return head :unauthorized if !video.owned_by(current_user)
 
-        video.story_cards.destroy
-        video.story_cards.create(params[:story_cards].accept(
-          :style, :left, :top, :width, :height,
-          :start_time, :end_time,
-          :title, :media, :image, :url
-        ))
+        video.story_cards.destroy_all
+
+        if params.key?(:video) && params[:video].key?(:cards)
+          card = params[:video][:cards].map{|c| c.permit(
+            :content_id, :content_type,
+            :style, :left, :top, :width, :height,
+            :start_time, :end_time,
+            :title, :media, :image, :url
+          )}
+
+          video.story_cards.create(card)
+        end
+        video.save
 
         return render json: {
           success: true
